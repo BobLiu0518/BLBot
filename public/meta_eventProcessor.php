58036aa2d404d19edf685f56bb4e26c5bb21622a
BobLiu <13636381320@163.com>
2018-12-27 18:53:13 +0800

    

--- public/meta_eventProcessor.php
+++ public/meta_eventProcessor.php
@@ -1,4 +1,16 @@
 <?php
     global $Queue;
-    $Queue[]= sendMaster('Get heartbeat signal');
+    //修正时区到日本
+    date_default_timezone_set('Asia/Tokyo');
+
+    $minute=(int)date('i');
+    $second=(int)date('s');
+    if($minute==0 && $second < 15)
+    {
+        $groups = array("761082692");
+        foreach($groups as $group_id){
+        $CQ->sendGroupMsg($group_id, CQCode::Record('base64://'.base64_encode(file_get_contents($dir."/storage/data/time/".$hour.".mp3"))));
+        $CQ->sendGroupMsg($group_id, file_get_contents($dir."/storage/data/time/".$hour.".txt"));
+    }
+}
 ?>
