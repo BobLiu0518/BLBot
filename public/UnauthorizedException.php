224c50a0cc96a8f1437ce06d15e176aa8842d559
BobLiu <13636381320@163.com>
2018-12-28 22:26:01 +0800

    

--- public/UnauthorizedException.php
+++ public/UnauthorizedException.php
@@ -1,17 +1,18 @@
 <?php
 namespace kjBot\Frame;
 
+global $Queue;
 class UnauthorizedException extends \Exception{
     function __construct(){
-        $img = sendImg(getData("dt/pd".rand(1,20).".jpg"));
+        $Queue[]= sendBack(sendImg(getData("dt/pd".rand(1,20).".jpg")));
         switch(rand(1,3))
         {
             case 1:
-            $this->message = $img."权限不足！停止你的瞎玩行为！";break;
+            $this->message = "权限不足！停止你的瞎玩行为！";break;
             case 2:
-            $this->message = $img."Permission denied...";break;
+            $this->message = "Permission denied...";break;
             case 3:
-            $this->message = $img."没有权限…一定是哪里不对头";break;
+            $this->message = "没有权限…一定是哪里不对头";break;
         }
         $this->code = 401;
     }
