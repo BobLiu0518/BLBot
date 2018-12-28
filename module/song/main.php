e78e1e0ad0192b9e7d6faf1ee6643b5b78b936c7
BobLiu <13636381320@163.com>
2018-12-28 21:04:07 +0800

    

--- module/song/main.php
+++ module/song/main.php
@@ -1,17 +1,20 @@
 <?php
 
-global $Queue, $CQ, $Text, $Event;
-
+global $Queue, $CQ, $Text, $Event, $User_id;
+loadModule('credit.tools');
 $wyyyy = true;
 
+decCredit($User_id,50);
+
 if(coolDown("song/user/{$Event['user_id']}")<0)leave('本命令每人每30秒只能使用一次！');
-coolDown("song/user/{$Event['user_id']}",30);
 
 if(fromGroup())
 {
     if(coolDown("song/group/{$Event['group_id']}")<0)leave('本命令每群每15秒只能使用一次！');
     coolDown("song/group/{$Event['group_id']}",15);
 }
+
+coolDown("song/user/{$Event['user_id']}",30);
 
 do{
 
@@ -55,7 +58,7 @@ if(!$wyyyy){
     $song = json_decode(file_get_contents($apiurl),true)['Body'][0];
 
     $mid = $song['mid'];
-    if(!$mid)leave('没有搜索到歌曲！');
+    if(!$mid){addCredit($User_id,50);leave('没有搜索到歌曲！');}
     $url = 'https://y.qq.com/n/yqq/song/'.$mid.'.html';
     //$audio = $song['url'];
     $audio = file_get_contents($song['url']);
@@ -63,11 +66,13 @@ if(!$wyyyy){
     $content = $song['author'].' / '.$song['album'];
     $image = $song['pic'];
     $Queue[]= sendBack('[CQ:music,type=custom,url='.$url.',audio='.$audio.',title='.$title.',content='.$content.',image='.$image.']');
+    $Queue[]= sendBack('点歌成功，扣除 50 金币，你的余额为 '.getCredit($User_id));
 }else{
     $apiurl = 'https://api.mlwei.com/music/api/wy/?key=523077333&id='.$Text.'&type=so&cache=0&nu=1';
     $id = json_decode(file_get_contents($apiurl),true)['Body'][0]['id'];
-    if(!$id)leave('没有搜索到歌曲！');
+    if(!$id){leave('没有搜索到歌曲！');addCredit($User_id,50);}
     $Queue[]= sendBack('[CQ:music,type=163,id='.$id.']');
+    $Queue[]= sendBack('点歌成功，扣除 50 金币，你的余额为 '.getCredit($User_id));
 }
 
 
