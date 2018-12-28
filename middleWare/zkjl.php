4c101a6e7a8deebba9041d0c7e86d483bb1230c9
BobLiu <13636381320@163.com>
2018-12-28 21:45:53 +0800

    

--- middleWare/zkjl.php
+++ middleWare/zkjl.php
@@ -3,11 +3,12 @@
 global $Message, $Queue;
 
 $zkjl = array("六学","说起","我就想到了","明年年初","中美合拍","正式开机","文体两开花","弘扬中国文化","多多关注","章口就莱");
+$zk = false;
 
 foreach($zkjl as $word)
     if(preg_match('/'.$word.'/', $Message))
-        $zkjl = true;
-if($zkjl)
+        $zk = true;
+if($zk)
     $Queue[]= sendBack(sendImg(getData("dt/zkjl.gif")));
 
 ?>
