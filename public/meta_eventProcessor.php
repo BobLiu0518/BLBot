0eff5ec6e7043f1977eb50a4691bb9b2f4eda5b3
BobLiu <13636381320@163.com>
2018-12-29 00:16:31 +0800

    

--- public/meta_eventProcessor.php
+++ public/meta_eventProcessor.php
@@ -1,17 +1,19 @@
 <?php
     global $Queue, $CQ;
-    //修正时区到日本
+    
     date_default_timezone_set('Asia/Tokyo');
 
     $minute=(int)date('i');
     $second=(int)date('s');
 
-    if($minute==0 && $second==0)
+    if($minute==0 && $second<=1)
     {
         $groups = array("761082692");
         foreach($groups as $group_id){
         $CQ->sendGroupMsg($group_id, CQCode::Record('base64://'.base64_encode(getData("time/".$hour.".mp3"))));
         $CQ->sendGroupMsg($group_id, getData("time/".$hour.".txt"));
     }
+
+    date_default_timezone_set('Asia/Shanghai');
 }
 ?>
