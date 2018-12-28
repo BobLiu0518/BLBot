637d8b875662dfed30670c92edd4ae4d91fa3123
BobLiu <13636381320@163.com>
2018-12-28 19:53:19 +0800

    

--- middleWare/zkjl.php
+++ middleWare/zkjl.php
@@ -1,7 +1,8 @@
 <?php
 
-global $Event, $Queue;
-if(preg_match('/^六学^/', $Event['message']) || preg_match('/^我就想到了^/', $Event['message']) || preg_match('/^文体两开花^/', $Event['message']) || preg_match('/^多多关注^/', $Event['message']) || preg_match('/^弘扬中国文化^/', $Event['message']) || preg_match('/^章口就莱^/', $Event['message']))
-    $Queue[]= sendBack("[CQ:image,file=F00437C9BFF54A80BB39270E14C2DB82.gif]");
+global $Message, $Queue;
+
+if(preg_match('/六学/', $Message) || preg_match('/我就想到了/', $Message) || preg_match('/文体两开花6/', $Message) || preg_match('/多多关注/', $Message) || preg_match('/弘扬中国文化/', $Message) || preg_match('/章口就莱/', $Message))
+    $Queue[]= sendBack(sendImg(getData("dt/zkjl.gif")));
 
 ?>
