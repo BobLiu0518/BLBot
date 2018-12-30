1295c2e3d27e619a0a2642b3f6d2c91f855afd7e
BobLiu <13636381320@163.com>
2018-12-30 18:57:11 +0800

    

--- module/ban/main.php
+++ module/ban/main.php
@@ -4,11 +4,16 @@ global $Queue;
 loadModule('ban.tools');
 requireSeniorAdmin();
 
+/*****/$Queue[]= sendBack("Debug: 加载命令成功！");
+
 $banList = loadBanList();
+/*****/$Queue[]= sendBack("Debug: 加载列表成功！");
 if($banId = nextArg())
 {
     $banList['Blacklist'][] = $banId;
+    /*****/$Queue[]= sendBack("Debug: 更改列表成功！");
     saveBanList($banList);
+    /*****/$Queue[]= sendBack("Debug: 保存列表成功！");
 }
 else
 {
