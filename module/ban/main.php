52cafb3e8c864ed48f8c06052e224adae06d0083
BobLiu <13636381320@163.com>
2018-12-30 19:02:25 +0800

    

--- module/ban/main.php
+++ module/ban/main.php
@@ -1,19 +1,19 @@
 <?php
 
-global $Queue;
+global $Queue, $CQ, $Event;
 loadModule('ban.tools');
 requireSeniorAdmin();
 
-/*****/$Queue[]= sendBack("Debug: 加载命令成功！");
+/*****/$CQ->sendGroupMsg($Event['group_id'], "Debug: 加载命令成功！");
 
 $banList = loadBanList();
-/*****/$Queue[]= sendBack("Debug: 加载列表成功！");
+/*****/$CQ->sendGroupMsg($Event['group_id'], "Debug: 加载列表成功！");
 if($banId = nextArg())
 {
     $banList['Blacklist'][] = $banId;
-    /*****/$Queue[]= sendBack("Debug: 更改列表成功！");
+    /*****/$CQ->sendGroupMsg($Event['group_id'], "Debug: 修改列表成功！");
     saveBanList($banList);
-    /*****/$Queue[]= sendBack("Debug: 保存列表成功！");
+    /*****/$CQ->sendGroupMsg($Event['group_id'], "Debug: 保存列表成功！");
 }
 else
 {
