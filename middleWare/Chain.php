98a00e1cdaccb0974a2fb8f648c7914bf9e097bf
BobLiu <13636381320@163.com>
2019-01-01 14:36:40 +0800

    

--- middleWare/Chain.php
+++ middleWare/Chain.php
@@ -4,12 +4,11 @@
 require('littleIce.php');
 require('zkjl.php');
 require('osu.php');
-require('version.php');
 require('ciya.php');
 require('checkin.php');
 require('song.php');
-require('repeat.php');
 require('bot.php');
 require('help.php');
+require('repeat.php');
 
 ?>
