0af48a55fff878d0bddf5884ffec9c90a9f81570
BobLiu <13636381320@163.com>
2019-01-01 17:39:51 +0800

    

--- public/UnauthorizedException.php
+++ public/UnauthorizedException.php
@@ -8,11 +8,11 @@ class UnauthorizedException extends \Exception{
         switch(rand(1,3))
         {
             case 1:
-            $this->message = $img."权限不足！停止你的瞎玩行为！";break;
+            $this->message = $img."\n权限不足！停止你的瞎玩行为！";break;
             case 2:
-            $this->message = $img."Permission denied...";break;
+            $this->message = $img."\nPermission denied...";break;
             case 3:
-            $this->message = $img."没有权限…一定是哪里不对头";break;
+            $this->message = $img."\n没有权限…一定是哪里不对头";break;
         }
         $this->code = 401;
     }
