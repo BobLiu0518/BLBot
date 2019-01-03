46f7595008cc7286cd35a812b2553955abf9ff3d
BobLiu <13636381320@163.com>
2019-01-03 13:54:21 +0800

    

--- middleWare/littleIce.php
+++ middleWare/littleIce.php
@@ -1,7 +1,7 @@
 <?php
 
 global $Event, $Queue;
-if($Event['user_id'] == "2854196306" && !preg_match("/@我说/", $Event['message']) && !preg_match("/艾特我说/", $Event['message']))
+if($Event['user_id'] == "2854196306" && !preg_match("/@我说/", $Event['message']) && !preg_match("/💡/", $Event['message']) && !preg_match("/艾特我说/", $Event['message']))
     leave("[CQ:at,qq=2854196306] 不玩了");
 else if($Event['user_id'] == "2854196306")
     leave();
