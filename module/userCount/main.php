55610ebc315cb460df25994f324abd7b6e7d7313
BobLiu <13636381320@163.com>
2019-01-05 17:48:30 +0800

    

--- module/userCount/main.php
+++ module/userCount/main.php
@@ -9,9 +9,8 @@ foreach($groupList as $group){
     foreach($memberList as $member)
         $idList[]= $member->user_id;
 }
-/*****/$Queue[]= sendBack("Debug: 4");
+$Queue[]= sendBack(var_export($idList));
 $idList = array_unique($idList);
-/*****/$Queue[]= sendBack("Debug: 5");
 $Queue[]= sendBack("本 Bot 一共加了 ".count($groupList)." 个群，群成员（去重）一共有".count($idList)."个");
 
 ?>
