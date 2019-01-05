b1826679f9e06f0e861c3a05e6e115c21472c478
BobLiu <13636381320@163.com>
2019-01-05 17:39:45 +0800

    

--- module/userCount/main.php
+++ module/userCount/main.php
@@ -2,13 +2,10 @@
 
 requireSeniorAdmin();
 global $CQ, $Queue;
-/*****/$Queue[]= sendBack("Debug: 1");
 $groupList = $CQ->getGroupList();
-/*****/$Queue[]= sendBack("Debug: 2");
 $idList = array();
-/*****/$Queue[]= sendBack("Debug: 3");
 foreach($groupList as $group){
-    $member = json_decode($CQ->getGroupMemberList($group));
+    $member = json_decode($CQ->getGroupMemberList($group->group_id));
     $id = $member->user_id;
     $idList[]= $id;
 }
