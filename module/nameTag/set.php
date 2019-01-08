d870e3be2fdbccdf6b9d1c21ca3ce66ceed7aea2
BobLiu <13636381320@163.com>
2019-01-08 22:00:16 +0800

    Bug fixes: nameTag

--- module/nameTag/set.php
+++ module/nameTag/set.php
@@ -2,11 +2,15 @@
 
 global $CQ, $Event, $Queue;
 
-if(!$tag = nextArg())leave('请输入需要的头衔！');
-if(mb_strlen($tag) > 6)leave('头衔过于长！');
-try{
+if(!fromGroup())
+    leave("只能在群聊中使用！");
+if($CQ->getGroupMemberInfo($Event['group_id'], config("bot","2094361499"))->role != "owner")
+    leave("没有权限！");
+else{
+    if(!$tag = nextArg())leave('请输入需要的头衔！');
+    if(mb_strlen($tag) > 6)leave('头衔过于长！');
     $CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id'], $tag);
-}catch(\Exception $e){leave("没有权限");}
-$Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已设置群头衔 '.$tag.' ！');
+    $Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已设置群头衔 '.$tag.' ！');
+}
 
 ?>
