3bc588d1aca4d5ab9115cf827c23342e4bdba3a6
BobLiu <13636381320@163.com>
2019-01-08 15:25:58 +0800

    Logic improve: 中间件逻辑改进

--- middleWare/osu.php
+++ middleWare/osu.php
@@ -4,23 +4,29 @@ global $CQ, $Event, $Queue;
 
 date_default_timezone_set('Asia/Shanghai');
 
-if(preg_match('/see you next time/', $Event['message'])){
-    if(isSeniorAdmin())leave();
-    try{
-        $CQ->setGroupBan($Event['group_id'], $Event['user_id'], strtotime(((date('H')>=0&&date('H')<=7)?'':'next day').' 7 am')-time());
-    }catch(\Exception $e){leave();}
-    
-}
-
-if(preg_match('/bot next door/', $Event['message'])){
-    if(!isSeniorAdmin())
+if(fromGroup())
+{
+    if(preg_match('/see you next time/', $Event['message']))
     {
-        try{
-            $CQ->setGroupKick($Event['group_id'], $Event['user_id']);
-            $Queue[]= sendBack(":D");
-        }catch(\Exception $e){leave();}
+        if(!isSeniorAdmin())
+        {
+            try
+            {
+                $CQ->setGroupBan($Event['group_id'], $Event['user_id'], strtotime(((date('H')>=0&&date('H')<=7)?'':'next day').' 7 am')-time());
+            }catch(\Exception $e){leave();}
+        } 
+    }
+    if(preg_match('/bot next door/', $Event['message']))
+    {
+        if(!isSeniorAdmin())
+        {
+            try
+            {
+                $CQ->setGroupKick($Event['group_id'], $Event['user_id']);
+                $Queue[]= sendBack(":D");
+            }catch(\Exception $e){leave();}
+        }
     }
 }
-
 
 ?>
