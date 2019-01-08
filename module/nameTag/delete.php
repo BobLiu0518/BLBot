c62ec4da8fe0934cb91dfc5b4af2ceee61b61f14
BobLiu <13636381320@163.com>
2019-01-08 22:00:16 +0800

    Bug fixes: nameTag

--- module/nameTag/delete.php
+++ module/nameTag/delete.php
@@ -2,9 +2,13 @@
 
 global $CQ, $Event, $Queue;
 
-try{
+if(!fromGroup())
+    leave("只能在群聊中使用！");
+if($CQ->getGroupMemberInfo($Event['group_id'], config("bot","2094361499"))->role != "owner")
+    leave("没有权限！");
+else{
     $CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id']);
-}catch(\Exception $e){leave("没有权限");}
-$Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已删除群头衔！');
+    $Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已删除群头衔！');
+}
 
 ?>
