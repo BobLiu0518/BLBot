816ac57340173b250bd938b1ce1bdae7f4e39cfd
BobLiu <13636381320@163.com>
2019-01-08 21:50:09 +0800

    Logic update: #nameTag will catch exception

--- module/nameTag/delete.php
+++ module/nameTag/delete.php
@@ -2,7 +2,9 @@
 
 global $CQ, $Event, $Queue;
 
-$CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id']);
+try{
+    $CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id']);
+}catch(\Exception $e){leave("没有权限");}
 $Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已删除群头衔！');
 
 ?>
--- module/nameTag/set.php
+++ module/nameTag/set.php
@@ -4,7 +4,9 @@ global $CQ, $Event, $Queue;
 
 if(!$tag = nextArg())leave('请输入需要的头衔！');
 if(mb_strlen($tag) > 6)leave('头衔过于长！');
-$CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id'], $tag);
+try{
+    $CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id'], $tag);
+}catch(\Exception $e){leave("没有权限");}
 $Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已设置群头衔 '.$tag.' ！');
 
 ?>
