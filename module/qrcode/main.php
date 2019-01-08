80370f65f0f32ef01642babaa584fc445b60fdc5
BobLiu <13636381320@163.com>
2019-01-08 17:27:20 +0800

    Feature improved: #qrcode

--- module/qrcode/main.php
+++ module/qrcode/main.php
@@ -6,11 +6,16 @@ while($nextArg = nextArg())
     $Text = $nextArg." ".$Text;
 if(!$Text)leave("没有内容！");
 if(strlen($Text) > 271)leave("消息过长！");
+$fee = (int)(mbstrlen($Text)/10+1);
 
-decCredit($User_id, strlen($Text)/10+1);
+decCredit($User_id, $fee);
 $link = "http://tool.oschina.net/action/qrcode/generate?output=image%2Fjpeg&error=L&type=0&margin=4&size=4&data=";
 $qr = file_get_contents($link.urlencode($Text));
+if(!$qr){
+    addCredit($User_id, $fee);
+    leave("生成失败！");
+}
 $Queue[]= sendBack(sendImg($qr));
-$Queue[]= sendBack("共 ".strlen($Text)." 个字节，已收取 ".(strlen($Text)/10+1)." 个金币，你的余额为 ".getCredit($User_id));
+$Queue[]= sendBack("共 ".strlen($Text)." 个字节，已收取 ".$fee." 个金币，你的余额为 ".getCredit($User_id));
 
 ?>
