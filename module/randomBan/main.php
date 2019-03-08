275e5a6711a184b45f405825a5800b0b58c48a77
Bob Liu <13636381320@163.com>
2019-03-08 16:18:31 +0800

    优化 #randomBan：如果禁言30天，会在 10分钟后解禁

--- module/randomBan/main.php
+++ module/randomBan/main.php
@@ -1,9 +1,16 @@
 <?php
+
 	global $Event, $CQ, $Queue;
 	$t = rand(0, 10*60);
-	if($t == 0)
+	if(!$t)
 		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], 2592000);
 	else
 		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], $t);
 	$Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已被禁言 '.intval($t / 60).'分钟'.($t % 60).'秒！');
+
+	if(!$t){
+		sleep(10*60);
+		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], 0);
+	}
+
 ?>
