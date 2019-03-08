390dbc8ad0bbdb15861b2c7572eb08571f90ce7b
Bob Liu <13636381320@163.com>
2019-03-08 17:12:19 +0800

    优化 #nameTag：如果使用 将不会检查头衔长度，如果头衔过长的话似乎会截取前六个字，但是系统提示里面字还是在的，自己玩⑧

--- module/nameTag/set.php
+++ module/nameTag/set.php
@@ -1,6 +1,7 @@
 <?php
 
 global $CQ, $Event, $Queue;
+$nocheck = false;
 
 if(!fromGroup())
     leave("只能在群聊中使用！");
@@ -8,7 +9,8 @@ if($CQ->getGroupMemberInfo($Event['group_id'], config("bot","2094361499"))->role
     leave("没有权限！");
 else{
     if(!$tag = nextArg())leave('请输入需要的头衔！');
-    if(mb_strlen($tag) > 6)leave('头衔过于长！');
+    if($tag == "--nocheck"){$nocheck = true; $tag = nextArg();}
+    if(mb_strlen($tag) > 6 && !$nocheck)leave('头衔过于长！');
     $CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id'], $tag);
     $Queue[]= sendBack('[CQ:at,qq='.$Event['user_id'].'] 已设置群头衔 '.$tag.' ！');
 }
