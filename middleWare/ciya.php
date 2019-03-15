4aa7c9be751ec6cebe3e184d7e5211d4b4b95099
Bob Liu <13636381320@163.com>
2019-03-15 23:47:15 +0800

    优化了呲牙功能！具体怎么玩…⑧要问我[CQ:face,id=13]

--- middleWare/ciya.php
+++ middleWare/ciya.php
@@ -1,20 +1,15 @@
 <?php
 
-use kjBot\SDK\CQCode;
-
 global $Event, $Queue, $Message;
 
 if(fromGroup()){
 
-$ciyaCount = (int)getData('ciyaCount/'.$Event['user_id'])+1;
-if(preg_match('/\[CQ:face,id=13\]/', $Message) && fromGroup()){
-    setData('ciyaCount/'.$Event['user_id'], $ciyaCount);
+	$ciyaCount = (int)getData('ciyaCount/'.$Event['user_id'])+1;
+	if(preg_match('/\[CQ:face,id=13\]/', $Message)){
+		setData('ciyaCount/'.$Event['user_id'], $ciyaCount);
+		if($ciyaCount % 5 == 0)
+			$Queue[]= sendBack(sendImg(getImg("ciya.gif")));
+	}
 }
 
-if($ciyaCount % 5 == 0){
-    $Queue[]= sendBack(CQCode::Face(13));
-    setData('ciyaCount/'.$Event['user_id'], $ciyaCount);
-}
-
-}
 ?>
