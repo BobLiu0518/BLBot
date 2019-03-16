c3ee4adc2932e8ee1dcd2fcc72daab43184c743e
Bob Liu <13636381320@163.com>
2019-03-17 01:50:01 +0800

    Update #bili.user: 可以查到更多数据了

--- module/version/main.php
+++ module/version/main.php
@@ -8,14 +8,9 @@ $ip = file_get_contents('ipv4.icanhazip.com');
 $ipv6 = file_get_contents('ipv6.icanhazip.com');
 
 $Queue[]= sendBack(<<<EOT
-BL1040Bot V1.0 (Based on kjBot)
+BL1040Bot V1.0 (Based on kjBot V2 legacy)
 
-以下为最近三个版本的版本记录：
-V1.0
-· 第一个版本
-
-非常感谢沙雕企鹅复读机下北泽一厂
-(群号761082692)群员提供的帮助！
+新功能请见：t.me/BLBot_New_Features
 EOT
 );
 
