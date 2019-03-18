bac0f96e4e84c7cd6ce51b862c84a5977564f1fc
Bob Liu <13636381320@163.com>
2019-03-18 17:10:24 +0800

    Fix bug: middleWare/bot.php 现在使用了 auto-escape

--- middleWare/bot.php
+++ middleWare/bot.php
@@ -5,8 +5,8 @@ global $Message, $User_id, $Queue, $CQ, $Event;
 if(preg_match('/机器人/', $Message)||parseQQ($Message) == config('bot')){
     if(config('master') == $User_id || config('devgroup') == $Event['group_id'])leave();
     $message=$User_id." in Group ".$Event['group_id']." says ".$Message;
-    $Queue[]= sendMaster($message);
-    $Queue[]= sendDevGroup($message);
+    $Queue[]= sendMaster($message, true);
+    $Queue[]= sendDevGroup($message, true);
 }
 if(parseQQ($Message) == config('bot')){
     if($Event['user_id'] == "80000000")leave("请不要使用匿名！");
