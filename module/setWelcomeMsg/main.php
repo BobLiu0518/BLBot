090ae6aa5cc49a69fbc5933fb85aa1d0a435318f
Bob Liu <13636381320@163.com>
2019-04-20 22:19:47 +0800

    Update feature: #setWelcomeMsg

--- module/setWelcomeMsg/main.php
+++ module/setWelcomeMsg/main.php
@@ -4,8 +4,12 @@ global $CQ, $Event, $Text;
 if($CQ->getGroupMemberInfo($Event['group_id'], $Event['user_id'])->role == 'member')
 	requireSeniorAdmin();
 
-if(!$Text)leave('请从第二行开始输入欢迎消息！');
-setData('addGroupMsg/'.$Event['group_id'], ' '.$Text);
-leave('设置成功！');
+if(!$Text){
+	delData('addGroupMsg/'.$Event['group_id'], ' '.$Text);
+	leave('恢复默认成功！');
+}else{
+	setData('addGroupMsg/'.$Event['group_id'], ' '.$Text);
+	leave('设置成功！');
+}
 
 ?>
