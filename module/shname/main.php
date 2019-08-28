caab5bb3679ce512cb4e88d35b66331076227c37
Bob Liu <13636381320@163.com>
2019-08-28 16:42:23 +0800

    Update #bili

---
+++ module/shname/main.php
@@ -0,0 +1,9 @@
+<?php
+
+$api = "https://shanghaicity.openservice.kankanews.com/citizen/repeat?name=";
+if(!$name = nextArg())leave("没有姓名！");
+
+$result = json_decode(file_get_contents($api.urlencode($name)), true);
+leave("截止今日，在上海市户籍人口中共有".$result['count']."个".$result['name']."信息。");
+
+?>
--- module/so/main.php
+++ module/so/main.php
@@ -1,28 +1,5 @@
 <?php
 
-global $Queue, $Event;
-loadModule('credit.tools');
-requireAdmin();
-
-$api = "http://music.163.com/api/search/pc?offset=1&limit=1&type=1&s=";
-$name = "";
-do{
-	$nextArg = nextArg();
-	$name .= " ".$nextArg;
-}while($nextArg);
-if(!$name)leave("没有歌曲名！");
-
-decCredit($Event['user_id'], 1000);
-$result = json_decode(file_get_contents($api.urlencode($name)), true)['result'];
-setData("debug/song", $result);
-$id = $result['songs'][0]['id'];
-
-if(!$id){
-	addCredit($Event['user_id'], 1000);
-	leave("点歌 ".$name." 失败！");
-}
-$Queue[]= sendBack("[CQ:music,type=163,id=".$id."]");
-if(fromGroup())$Queue[]= sendPM("[CQ:music,type=163,id=".$id."]");
-leave("点歌成功，扣除1000金币！");
+loadModule("so.163");
 
 ?>
