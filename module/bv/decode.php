8abba97a9cf6bfd190cf76895cb4e51c05c8d03d
Bob Liu <13636381320@163.com>
2020-03-24 06:15:21 +0800

    New feature: #bv

---
+++ module/bv/decode.php
@@ -0,0 +1,22 @@
+<?php
+
+global $Queue;
+
+$bv = nextArg();
+if(strpos($bv, 'BV') !== 0)leave('请输入有效的BV号！');
+
+$table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
+$tr = array();
+for($i = 0; $i < 58; $i += 1)
+        $tr[$table[$i]] = $i;
+$s = array(11, 10, 3, 8, 4, 6);
+$xorVal = 177451812;
+$addVal = 8728348608;
+$r = 0;
+for($i = 0; $i < 6; $i += 1)
+	$r += $tr[$bv[$s[$i]]] * pow(58, $i);
+$av = ($r - $addVal) ^ $xorVal;
+
+$Queue[]= sendBack('av'.$av);
+
+?>
