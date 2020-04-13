fbb80e7b902581ce39c02b8ca15a0435e2f694e6
Bob Liu <13636381320@163.com>
2020-04-14 04:34:32 +0800

    New feature: #yxh

--- public/meta_eventProcessor.php
+++ public/meta_eventProcessor.php
@@ -1,49 +1,23 @@
 <?php
-    global $Queue, $CQ;
-    use kjBot\SDK\CQCode;
 
-    date_default_timezone_set('Asia/Tokyo');
+global $Queue, $CQ;
+use kjBot\SDK\CQCode;
 
-    $hour = (int)date('H');
-    $minute = (int)date('i');
-    $second = (int)date('s');
+date_default_timezone_set("Asia/Shanghai");
+$hour = (int)date('g');
+$minute = (int)date('i');
+$second = (int)date('s');
+/*
+if($hour == 9 && $minute == 42 && $second <= 1){
+    $groups = array("1059156858");
+    foreach($groups as $group_id)
+        $CQ->sendGroupMsg($group_id, '叮');
+}
 
-    if($minute == 0 && $second <= 1)
-    {
-        $groups = array(/*"761082692"*/,"967313330","314353477","496111478");
-        foreach($groups as $group_id){
-            $CQ->sendGroupMsg($group_id, getData("time/".$hour.".txt"));
-            $CQ->sendGroupMsg($group_id, CQCode::Record('base64://'.base64_encode(getData("time/".$hour.".mp3"))));
-        }
-    }
-
-    date_default_timezone_set('Asia/Shanghai');
-
-    $hour = (int)date('H');
-
-    if($minute >= 37 && $minute < 40 && ($hour == 10 || $hour == 22) && $second <= 1){
-        $groups = array("772503459");
-        foreach($groups as $group_id)
-            $CQ->sendGroupMsg($group_id, '滴，滴，离发车时间还有'.(40 - $minute).'分钟');
-    }
-
-    if($minute == 40 && ($hour == 10 || $hour == 22) && $second <= 1){
-        $groups = array("772503459");
-        foreach($groups as $group_id)
-            $CQ->sendGroupMsg($group_id, '滴，滴，请发车');
-    }
-
-    if($minute == 41 && ($hour == 10 || $hour == 22) && $second <= 1){
-        $groups = array("772503459");
-        foreach($groups as $group_id)
-            $CQ->sendGroupMsg($group_id, '滴，滴，车辆出场，以下漏乖');
-    }
-
-    /*if(($hour==10 || $hour==22)&& $minute==40 &&($second==30 || $second==31))
-    {
-        $groups = array("761082692");
-        foreach($groups as $group_id)
-            $CQ->sendGroupMsg($group_id, CQCode::Record('base64://'.base64_encode(getData("rcd/Gota_del_Vient.mp3"))));
-    }*/
-
+if($hour == 9 && $minute == 43 && $second <= 1){
+    $groups = array("1059156858");
+    foreach($groups as $group_id)
+        $CQ->sendGroupMsg($group_id, '↓→↗↑ 欢迎乘坐942路公交车 方向 殷行路中原路');
+}
+*/
 ?>
