144e38700ef307c6aaa72eb92be5e55996f2600f
BobLiu <bobliu@bobliu.tech>
2023-02-04 20:27:12 +0800

    Update #jrrp #mrrp

--- module/mrrp/main.php
+++ module/mrrp/main.php
@@ -1,15 +1,13 @@
 <?php
 
 global $Event, $Queue, $CQ;
-requireLvl(0);
+requireLvl(1);
 
-$str = date("Ymd", (time() + 86400)).$Event['user_id'];
-$hash = hexdec(crc32($str));
-$mrrp = ($hash % 150) + 1;
-if($mrrp > 100) $mrrp -= 50;
+loadModule('jrrp.tools');
+$mrrp = getRp($Event['user_id'], time() + 86400);
 
-$offsetP = hexdec(crc32($str.'offsetP')) % 15;
-$offsetN = hexdec(crc32($str.'offsetN')) % 15;
+$offsetP = hexdec(crc32($str.'offsetP')) % 16;
+$offsetN = 15 - $offsetP;
 
 $Queue[]= replyMessage("Bot 觉得你明天的人品大概在 ".(($mrrp - $offsetN) <= 0 ? 0 : ($mrrp - $offsetN))."~".(($mrrp + $offsetP) > 100? 100: ($mrrp + $offsetP))." 之间");
 
