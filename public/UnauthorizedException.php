d8b197331153be0d18b596c3cdc190039260e97b
BobLiu <bobliu@bobliu.tech>
2023-02-06 18:38:26 +0800

    Update: Unauthorized Exception

--- public/UnauthorizedException.php
+++ public/UnauthorizedException.php
@@ -3,6 +3,7 @@ namespace kjBot\Frame;
 
 class UnauthorizedException extends \Exception{
     function __construct(){
+        global $Event;
         $img = sendImg(getImg("pd".rand(1,23).".jpg"));
         switch(rand(1,3))
         {
@@ -13,6 +14,7 @@ class UnauthorizedException extends \Exception{
             case 3:
             $this->message = $img."\n没有权限…一定是哪里不对头";break;
         }
+        $this->message = '[CQ:reply,id='.$Event['message_id'].']'.$this->message;
         $this->code = 401;
     }
 }
