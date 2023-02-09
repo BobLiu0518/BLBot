3ae27c941c2ea2565cf614c45a3a9dfa1909885e
BobLiu <bobliu@bobliu.tech>
2023-02-10 00:13:44 +0800

    Update #qrcode

--- module/qrcode/main.php
+++ module/qrcode/main.php
@@ -1,22 +1,19 @@
 <?php
 
-global $Queue, $Text, $User_id;
+global $Queue, $Text, $User_id, $Command;
+requireLvl(1);
 loadModule('credit.tools');
-while($nextArg = nextArg())
-    $Text = $nextArg." ".$Text;
-if($Text === "")replyAndLeave("没有填写要生成的内容呢OvO");
-if(strlen($Text) > 271)replyAndLeave("消息太长啦！QwQ");
-if(strpos($Text, "[CQ:") !== false)replyAndLeave("二维码生成只支持纯文本哦");
-$fee = intval(mb_strlen($Text, 'utf-8'))*100;
 
-decCredit($User_id, $fee);
-$link = "http://tool.oschina.net/action/qrcode/generate?output=image%2Fjpeg&error=L&type=0&margin=15&size=4&data=";
-$qr = file_get_contents($link.urlencode($Text));
-if(!$qr){
-    addCredit($User_id, $fee);
-    replyAndLeave("生成失败呜呜呜");
-}
-$Queue[]= sendBack(sendImg($qr));
-$Queue[]= replyMessage("共 ".strlen($Text)." 字节，已收取 ".$fee." 金币，你的余额为 ".getCredit($User_id));
+$text = trim(implode(' ', array_slice($Command, 1))."\n".$Text);
+if(trim($text) === "")replyAndLeave("没有填写要生成的内容呢OvO");
+if(strlen($text) > 271)replyAndLeave("消息太长啦！QwQ");
+if(strpos($text, "[CQ:") !== false)replyAndLeave("二维码生成只支持纯文本哦");
+$fee = intval(mb_strlen($text, 'utf-8'))*100;
+// decCredit($User_id, $fee);
+
+$qr = sendImg((new SimpleSoftwareIO\QrCode\Generator)->encoding('UTF-8')->format('png')->size(512)->margin(2)->style('round')->generate($text));
+
+$Queue[]= sendBack($qr);
+$Queue[]= replyMessage("共 ".mb_strlen($text, 'utf-8')." 字符，已收取 ".$fee." 金币，你的余额为 ".getCredit($User_id));
 
 ?>
