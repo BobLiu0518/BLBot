c6c5660f629b79271fa8207fbb38b926cdba3ac4
BobLiu <bobliu@bobliu.tech>
2023-02-11 21:23:42 +0800

    Update #me

--- module/credit/check.php
+++ module/credit/check.php
@@ -1,6 +1,6 @@
 <?php
 
-global $Queue, $Event;
+global $Queue, $Event, $CQ;
 use kjBot\SDK\CQCode;
 loadModule('credit.tools');
 loadModule('exp.tools');
@@ -12,15 +12,32 @@ if(!(preg_match('/\d+/', $QQ, $match) && $match[0] == $QQ)){
 }
 $QQ = $QQ??$Event['user_id'];
 
+if($Event['user_id'] != $QQ){
+	if(!fromGroup()){
+		replyAndLeave('只能查询自己和群员的信息哦…');
+	}
+	$inGroup = false;
+	foreach($CQ->getGroupMemberList($Event['group_id']) as $groupMember){
+		if($groupMember->user_id == $QQ){
+			$inGroup = true;
+		}
+	}
+	if(!$inGroup){
+		replyAndLeave($QQ.' 不在本群哦…');
+	}
+}
+
 $exp = getExp($QQ);
 $level = getLvl($QQ);
 $status = getStatus($QQ);
 $statusEnd = getStatusEndTime($QQ);
-$msg = "您的金币余额为 ".getCredit($QQ)."\n您的经验值为 ".$exp."，等级为 Lv".$level;
-switch($level) {
-	case 2: $msg .= "\n再签到 ".(30-$exp)." 天即可升级 Lv3～"; break;
-	case 1: $msg .= "\n再签到 ".(7-$exp)." 天即可升级 Lv2～"; break;
-	case 0: $msg .= "\n签到后即可升级 Lv1 哦～"; break;
+$msg = "您的金币余额为 ".getCredit($QQ)."，经验值为 ".$exp."，等级为 Lv".$level." ～";
+if($Event['user_id'] == $QQ){
+	switch($level) {
+		case 2: $msg .= "\n再签到 ".(30-$exp)." 天即可升级 Lv3～"; break;
+		case 1: $msg .= "\n再签到 ".(7-$exp)." 天即可升级 Lv2～"; break;
+		case 0: $msg .= "\n签到后即可升级 Lv1 哦～"; break;
+	}
 }
 switch($status) {
 	case 'imprisoned': $msg .= "\n当前身处监狱中，预计 ".$statusEnd." 出狱"; break;
@@ -28,6 +45,10 @@ switch($status) {
 	case 'arknights': $msg .= "\n当前身处异世界"; break;
 	case 'hospitalized': $msg .= "\n当前身处医院中，预计 ".$statusEnd." 出院"; break;
 	case 'free': default: break;
+}
+
+if($Event['user_id'] != $QQ){
+	$msg = str_replace('您', '@'.($CQ->getGroupMemberInfo($Event['group_id'], $QQ)->card ? $CQ->getGroupMemberInfo($Event['group_id'], $QQ)->card : $CQ->getGroupMemberInfo($Event['group_id'], $QQ)->nickname).' ', $msg);
 }
 $Queue[]= replyMessage($msg);
 
