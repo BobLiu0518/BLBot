1fcbff49960fa1432ebfa55080c47088add0a39d
BobLiu <bobliu@bobliu.tech>
2023-03-06 21:20:03 +0800

    New feature: #calc

---
+++ module/calc/main.php
@@ -0,0 +1,27 @@
+<?php
+
+requireLvl(1);
+
+global $Queue, $Message, $Text;
+$args = '';
+while($nextArg = nextArg()){
+	$args .= ' '.$nextArg;
+}
+$exp = trim($args.$Text);
+if(!$exp){
+	if(!$Message){
+		replyAndLeave('想让 Bot 算什么呢？');
+	}
+	$exp = $Message;
+}
+
+use NXP\MathExecutor;
+$executor = new MathExecutor();
+
+try{
+	$Queue[]= replyMessage($exp.' = '.$executor->execute($exp));
+}catch(\Exception $e){
+	replyAndLeave('计算 '.$exp.' 时发生错误：'.preg_replace('/(?<!^)((?<![[:upper:]])[[:upper:]]|[[:upper:]](?![[:upper:]]))/', ' $1', str_replace('Exception', '', array_pop(explode('\\', get_class($e))))));
+}
+
+?>
