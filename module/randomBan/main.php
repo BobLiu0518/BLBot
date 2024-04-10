edceb0c228549908bdd8e62e9f15d28d20c7781b
BobLiu <bobliu@bobliu.tech>
2024-04-10 22:29:50 +0800

    New feature: #sun #attack.rand

--- module/randomBan/main.php
+++ module/randomBan/main.php
@@ -1,27 +1,19 @@
 <?php
 
-if(!fromGroup())leave('请在群聊中使用！');
+global $Event, $CQ, $Queue;
+loadModule('randomBan.tools');
 
-	global $Event, $CQ, $Queue;
-	$t = rand(0, 10*60);
-	$role = $CQ->getGroupMemberInfo($Event['group_id'], $Event['user_id'], true)->role;
-	$bot = $CQ->getGroupMemberInfo($Event['group_id'], config('bot'), true)->role;
-	if($role == 'owner' || ($role == 'admin' && $bot == 'member')){
-		replyAndLeave("身为".($role == 'owner'?"群主":"管理员")."就可以这样调戏我嘛？");
-	}else if($bot == 'member'){
-		replyAndLeave("Bot 不是管理员呜呜呜");
-	}else if($bot == 'admin' && $role == 'admin'){
-		replyAndLeave("您已被禁…等下，你也是管理？".(rand(0,5)?'':'那要不你就假装被禁言 '.intval($t / 60).'分'.($t % 60).'秒吧～'));
-	}
-	if(!$t)
-		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], 2592000);
-	else
-		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], $t);
-	$Queue[]= replyMessage('您已被禁言 '.intval($t / 60).'分'.($t % 60).'秒～');
+$role = $CQ->getGroupMemberInfo($Event['group_id'], $Event['user_id'], true)->role;
+$bot = $CQ->getGroupMemberInfo($Event['group_id'], config('bot'), true)->role;
+if($role == 'owner' || ($role == 'admin' && $bot == 'member')){
+	replyAndLeave("身为".($role == 'owner'?"群主":"管理员")."就可以这样调戏我嘛？");
+}else if($bot == 'member'){
+	replyAndLeave("Bot 不是管理员呜呜呜");
+}else if($bot == 'admin' && $role == 'admin'){
+	replyAndLeave("您已被禁…等下，你也是管理？".(rand(0,5)?'':'那要不你就假装被禁言 '.intval($t / 60).'分'.($t % 60).'秒吧～'));
+}
 
-	if(!$t){
-		sleep(10*60);
-		$CQ->setGroupBan($Event['group_id'], $Event['user_id'], 0);
-	}
+$t = randomBan();
+$Queue[]= replyMessage('您已被禁言 '.intval($t / 60).'分'.($t % 60).'秒～');
 
 ?>
---
+++ module/sun/tools.php
@@ -0,0 +1,27 @@
+<?php
+
+function getRandGroupMember(){
+	date_default_timezone_set('Asia/Shanghai');
+	global $Event, $CQ;
+
+	$time = time();
+	$memberList = $CQ->getGroupMemberList($Event['group_id']);
+	$pool = [];
+
+	foreach($memberList as $member) {
+	    if($time - $member->last_sent_time <= 86400 * 3) {
+	        $pool[] = $member;
+	    }
+	}
+	if(!count($pool)) {
+	    $pool = $memberList;
+	}
+	$member = $pool[rand(0, count($pool)-1)];
+
+	return [
+		'nickname' => $member->card ? $member->card : $member->nickname,
+		'user_id' => $member->user_id,
+	];
+}
+
+?>
