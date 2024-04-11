05450e136ddb76c2d9de0e608e03f0bbc617d5b5
BobLiu <bobliu@bobliu.tech>
2024-04-11 16:19:19 +0800

    Revert "Fix deperated usage"
    This reverts commit b09d34191e2e2c23b7a2fbbb5ab9442e05d4b74d.

--- module/alias/tools.php
+++ module/alias/tools.php
@@ -4,7 +4,6 @@ if(!config("alias",false))leave('功能不开放');
 
 function setAlias($qq, $from, $to){
 	$list = json_decode(getData("alias/".$qq.".json"),true);
-	if(!$list) $list = [];
 	if(count($list) >= 4){
 		requireLvl(3, '设置更多别名', '使用 #alias.del 删掉一些');
 		if(count($list) >= 8){
