8af15c9f5c5d589dcfad5d607610bc8c174e8b17
BobLiu <bobliu@bobliu.tech>
2024-04-15 13:27:01 +0800

    Update #alias

--- module/alias/check.php
+++ module/alias/check.php
@@ -1,14 +1,28 @@
 <?php
 
-	global $Queue, $Event;
+	global $Event;
 	loadModule('alias.tools');
 	$list = chkAlias($Event['user_id']);
-	$w = "你设置的别名：\n";
-	if(count($list))
-		foreach($list as $alias => $command)
-			$w .= " #".$alias." => #".$command."\n";
-	else
-		$w = "你还没有设置别名哦～";
-	$Queue[]= replyMessage(rtrim($w));
+	if(count($list)){
+		$reply = '你设置的别名：';
+		$aliases = [];
+		foreach($list as $alias => $command){
+			$aliases[] = [
+				'alias' => $alias,
+				'command' => $command,
+			];
+		}
+		usort($aliases, function($a, $b){
+			if($result = strcmp($a['command'], $b['command'])) return $result;
+			return strcmp($a['alias'], $b['alias']);
+		});
+		foreach($aliases as $alias){
+			$reply .= "\n#".$alias['alias'].' => #'.$alias['command'];
+		}
+		replyAndLeave($reply);
+	}
+	else{
+		replyAndLeave('你还没有设置别名哦～');
+	}
 
 ?>
