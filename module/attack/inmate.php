733590d9195612b577aeb4b09d694a6bbe8c8917
BobLiu <bobliu@bobliu.tech>
2024-04-15 08:56:35 +0800

    New feature: #inmate

---
+++ module/attack/inmate.php
@@ -0,0 +1,21 @@
+<?php
+
+global $CQ, $Event;
+requireLvl(1);
+loadModule('attack.tools');
+$memberList = $CQ->getGroupMemberList($Event['group_id']);
+$groupName = $CQ->getGroupInfo($Event['group_id'])->group_name;
+$inmates = [];
+foreach($memberList as $member){
+	$status = getStatus($member->user_id);
+	if($status == 'imprisoned' || $status == ''){
+		$inmates[] = '@'.($member->card ? $member->card : $member->nickname);
+	}
+}
+if(count($inmates)){
+	replyAndLeave($groupName."狱友：\n".implode("\n", $inmates));
+}else{
+	replyAndLeave('群监狱现在没有人哦…');
+}
+
+?>
