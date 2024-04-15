f4c450c7901a788fb51303e3a7942645aab645af
BobLiu <bobliu@bobliu.tech>
2024-04-15 13:19:41 +0800

    Update #inmate

--- module/attack/inmate.php
+++ module/attack/inmate.php
@@ -9,11 +9,23 @@ $inmates = [];
 foreach($memberList as $member){
 	$status = getStatus($member->user_id);
 	if($status == 'imprisoned' || $status == 'confined'){
-		$inmates[] = '@'.($member->card ? $member->card : $member->nickname)."\n　刑期至 ".getStatusEndTime($member->user_id);
+		$inmates[] = [
+			'nickname' => ($member->card ? $member->card : $member->nickname),
+			'end' => getStatusEndTime($member->user_id),
+		];
 	}
 }
 if(count($inmates)){
-	replyAndLeave($groupName."狱友：\n".implode("\n", $inmates));
+	usort($inmates, function($a, $b){
+		if($a['end'] == '∞') return 1;
+		else if($b['end'] == '∞') return -1;
+		return $a['end'] - $b['end'];
+	});
+	$reply .= $groupName.' 狱友：';
+	foreach($inmates as $inmate){
+		$reply .= "\n@".$inmate['nickname']."\n　刑期至：".$inmate['end'];
+	}
+	replyAndLeave($reply);
 }else{
 	replyAndLeave('群监狱现在没有人哦…');
 }
