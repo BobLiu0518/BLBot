80f46851c921b3981a760db826cfba824317339a
BobLiu <bobliu@bobliu.tech>
2024-04-17 18:12:12 +0800

    Fix deperated && Fix small bugs

--- middleWare/alias.php
+++ middleWare/alias.php
@@ -1,16 +1,8 @@
 <?php
 
-global $Event, $Command, $Text;
+global $Event;
 
-$length = strpos($Event['message'], "\r");
-if(false===$length)$length = strlen($Event['message']);
-$Command = parseCommand(substr($Event['message'], strlen(Config('prefix', '#'))-1, $length));
-$Text = substr($Event['message'], $length+2);
-
-$module = substr(nextArg(), strlen(Config('prefix', '#')));
-$command = explode(" ", $Event['message'])[0];
-if(config('alias',false) == true && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'),true)[$command]){
-	//$Queue[]= sendBack("alias: redirect to ".$alias);
+if(config('alias',false) == true && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'),true)[nextArg()]){
 	loadModule($alias);
 	leave();
 }
--- public/messageProcessor.php
+++ public/messageProcessor.php
@@ -1,21 +1,19 @@
 <?php
 
 $message = trim(str_replace('[CQ:at,qq='.config('bot').']', '', $Event['message']));
-//$message = $Event['message'];
-if(fromGuild()){
-    require('./guildProcessor.php');
-}else if(preg_match('/^('.config('prefix', '#').')/', $message, $prefix)
+$length = strpos($message, "\n");
+if(false===$length){
+    $length = strlen($message);
+}
+
+if(preg_match('/^('.config('prefix', '#').')/', $message, $prefix)
     || preg_match('/^('.config('prefix2', '.').')/', $message, $prefix) && config('enablePrefix2', false)){
-    $length = strpos($message, "\n");
-    if(false===$length)$length = strlen($message);
     $Command = parseCommand(substr($message, strlen($prefix[1])-1, $length));
     $Text = substr($message, $length+1);
     $module = substr(nextArg(), strlen($prefix[1]));
     try{
         if(config('alias',false) == true
-            && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'),true)[$module])
-        {
-            //$Queue[]= sendBack("alias: redirect to ".$alias);
+            && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'),true)[$module]){
             loadModule($alias);
         }
         else
@@ -25,10 +23,8 @@ if(fromGuild()){
     }
 }else{ //不是命令
     $Message = $message;
-    $length = strpos($message, "\r");
-    if(false===$length)$length = strlen($message);
-    $Command = parseCommand(substr($message, strlen(Config('prefix', '#'))-1, $length));
-    $Text = substr($message, $length+2);
+    $Command = parseCommand(substr($message, 0, $length));
+    $Text = substr($message, $length+1);
     require('../middleWare/Chain.php');
 }
 
