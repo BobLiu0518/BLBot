6b9cc0d99126e27711fa72532d84becd35cd3eba
BobLiu <bobliu@bobliu.tech>
2024-04-21 17:05:43 +0800

    Update #toilet: Add Beijing subway data

--- module/toilet/shanghai.php
+++ module/toilet/shanghai.php
@@ -3,20 +3,21 @@
 $stations = json_decode(file_get_contents('https://m.shmetro.com/core/shmetro/mdstationinfoback_new.ashx?act=getAllStations'), true);
 $stationInfoApi = 'https://m.shmetro.com/interface/metromap/metromap.aspx?func=stationInfo&stat_id=';
 $data = json_decode(getData('toilet/data.json'), true);
-$data['上海'] = [];
+setCache('toilet/'.time().'.bak', json_encode($data));
+$data['上海地铁'] = [];
 
 foreach($stations as $station){
 	$stationInfo = json_decode(file_get_contents($stationInfoApi.$station['key']), true)[0];
 	$toiletInfo = json_decode($stationInfo['toilet_position'], true)['toilet'];
-	$data['上海'][$station['value']] = [];
+	$data['上海地铁'][$station['value']] = [];
 	foreach($toiletInfo as $toiletInfoDetail){
 		if($toiletInfoDetail['lineno'] == 41) $toiletInfoDetail['lineno'] = '浦江线';
-		$data['上海'][$station['value']][] .= '【'.(is_numeric($toiletInfoDetail['lineno']) ? $toiletInfoDetail['lineno'].'号线' : $toiletInfoDetail['lineno'])."】".$toiletInfoDetail['description'];
+		$data['上海地铁'][$station['value']][] .= '【'.(is_numeric($toiletInfoDetail['lineno']) ? $toiletInfoDetail['lineno'].'号线' : $toiletInfoDetail['lineno'])."】".$toiletInfoDetail['description'];
 	}
-	$data['上海'][$station['value']] = implode("\n", $data['上海'][$station['value']]);
+	$data['上海地铁'][$station['value']] = implode("\n", $data['上海地铁'][$station['value']]);
 }
 
 setData('toilet/data.json', json_encode($data));
-replyAndLeave('更新数据成功，共 '.count($data['上海']).' 条数据');
+replyAndLeave('更新数据成功，共 '.count($data['上海地铁']).' 条数据');
 
 ?>
