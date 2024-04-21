fab4c899f175b08b74cc59bb21c6bb3a6aed9670
BobLiu <bobliu@bobliu.tech>
2024-04-21 23:16:06 +0800

    Update #toilet: Add fuzzy matching

--- module/toilet/main.php
+++ module/toilet/main.php
@@ -2,6 +2,26 @@
 
 global $Message;
 
+function utf8_to_extended_ascii($str, &$map){
+	$matches = array();
+	if (!preg_match_all('/[\xC0-\xF7][\x80-\xBF]+/', $str, $matches)){
+		return $str;
+	}
+	foreach ($matches[0] as $mbc){
+		if (!isset($map[$mbc])){
+			$map[$mbc] = chr(128 + count($map));
+		}
+	}
+
+	return strtr($str, $map);
+}
+function levenshtein_utf8($s1, $s2){
+	$charMap = array();
+	$s1 = utf8_to_extended_ascii($s1, $charMap);
+	$s2 = utf8_to_extended_ascii($s2, $charMap);
+	return levenshtein($s1, $s2);
+}
+
 $station = nextArg();
 if(!$station) $station = $Message;
 if(!$station) replyAndLeave('要查询什么车站呢？');
@@ -13,9 +33,30 @@ foreach($data as $companyName => $company){
 	$companies[] = $companyName;
 }
 if(!strlen($reply)){
-	replyAndLeave('没有查询到名为 '.$station." 的车站哦…\n目前仅支持".implode('、', $companies).'的车站洗手间查询哦（更多城市接入中…）');
-}else{
-	replyAndLeave(trim($reply));
+	$similarNames = [];
+	foreach($data as $companyName => $company){
+		foreach($company as $stationName => $stationInfo){
+			$strDistance = levenshtein_utf8($station, $stationName);
+			if($strDistance <= min(4, mb_strlen($station, 'UTF-8') / 2)){
+				$similarNames[] = [
+					'name' => $stationName,
+					'distance' => $strDistance,
+				];
+			}
+		}
+	}
+	$reply = '没有查询到名为 '.$station." 的车站哦…\n目前仅支持".implode('、', $companies).'的车站洗手间查询哦（更多城市接入中…）';
+	if(count($similarNames)){
+		usort($similarNames, function($a, $b){
+			return $a['distance'] - $b['distance'];
+		});
+		$reply .= "\n你可能想找：";
+		foreach($similarNames as $name){
+			$reply .= $name['name'].' ';
+		}
+	}
 }
+
+replyAndLeave(trim($reply));
 
 ?>
