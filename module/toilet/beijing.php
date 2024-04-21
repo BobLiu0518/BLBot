408f506355f943de9d3bf8fdba8d140af96753d5
BobLiu <bobliu@bobliu.tech>
2024-04-21 17:05:43 +0800

    Update #toilet: Add Beijing subway data

---
+++ module/toilet/beijing.php
@@ -0,0 +1,40 @@
+<?php
+
+// 该 API 需要验证 sign，尚不清楚计算机制
+// $lines = json_decode(file_get_contents('https://96123.bmncc.com.cn/bjtt-subway-app/api/baseline/queryAllLinesChildStations'), true)['r'];
+$lines = json_decode(getCache('toilet/beijingStations.json'), true)['r'];
+$lines = json_decode(base64_decode($lines), true)['result'];
+$stationInfoApi = 'https://96123.bmncc.com.cn/bjtt-subway-app/api/inside/info';
+$data = json_decode(getData('toilet/data.json'), true);
+setCache('toilet/'.time().'.bak', json_encode($data));
+$data['北京轨道交通'] = [];
+
+foreach($lines as $line){
+	foreach($line['listStations'] as $station){
+		if($data['北京轨道交通'][$station['stationNameCn']]) continue;
+		$options = [
+			'http' => [
+				'method' => 'POST',
+				'header' => 'Content-Type: application/json',
+				'content' => json_encode(['stationName' => $station['stationNameCn']]),
+			],
+		];
+		$context = stream_context_create($options);
+		$stationInfo = json_decode(file_get_contents($stationInfoApi, false, $context), true)['r'];
+		$stationInfo = json_decode(base64_decode($stationInfo), true)['result'];
+		foreach($stationInfo['insideInfoList'] as $insideInfo){
+			if($insideInfo['insideCode'] == 'SF_TOILET'){
+				$data['北京轨道交通'][$station['stationNameCn']] = [];
+				foreach($insideInfo['insideInfoDesc'] as $insideInfoDesc){
+					$data['北京轨道交通'][$station['stationNameCn']][] = '【'.$insideInfoDesc['lineName'].'】'.$insideInfoDesc['insideDesc'];
+				}
+				$data['北京轨道交通'][$station['stationNameCn']] = implode("\n", $data['北京轨道交通'][$station['stationNameCn']]);
+			}
+		}
+	}
+}
+
+setData('toilet/data.json', json_encode($data));
+replyAndLeave('更新数据成功，共 '.count($data['北京轨道交通']).' 条数据');
+
+?>
--- module/toilet/main.php
+++ module/toilet/main.php
@@ -7,11 +7,13 @@ if(!$station) $station = $Message;
 if(!$station) replyAndLeave('要查询什么车站呢？');
 $data = json_decode(getData('toilet/data.json'), true);
 $reply = '';
-foreach($data as $cityName => $city){
-	if($city[$station]) $reply .= "\n".$cityName.'地铁 '.$station." 站\n".$city[$station];
+$companies = [];
+foreach($data as $companyName => $company){
+	if($company[$station]) $reply .= "\n".$companyName.' '.$station." 站：\n".$company[$station];
+	$companies[] = $companyName;
 }
 if(!strlen($reply)){
-	replyAndLeave('没有查询到名为 '.$station.' 的车站哦…');
+	replyAndLeave('没有查询到名为 '.$station." 的车站哦…\n目前仅支持".implode('、', $companies).'的车站洗手间查询哦（更多城市接入中…）');
 }else{
 	replyAndLeave(trim($reply));
 }
