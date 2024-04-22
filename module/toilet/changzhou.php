e9fd67bff4347eda92d04d1f1095591c4732210c
BobLiu <bobliu@bobliu.tech>
2024-04-22 17:20:32 +0800

    Update #toilet: Fix bugs

--- module/toilet/changzhou.php
+++ module/toilet/changzhou.php
@@ -14,7 +14,9 @@ setCache('toilet/'.time().'.bak', json_encode($data));
 $data['常州地铁'] = [];
 foreach($lines as $line){
 	foreach($line['station_data'] as $station){
-		$station['name'] = preg_replace('/站$/', '', $station['name']);
+		if(!in_array($station['name'], ['常州火车站', '常州北站'])){
+			$station['name'] = preg_replace('/站$/', '', $station['name']);
+		}
 		$stationInfo = json_decode(file_get_contents($stationInfoApi.$station['id'], false, $context), true)['data'];
 		$data['常州地铁'][$station['name']] = ($data['常州地铁'][$station['name']] ? $data['常州地铁'][$station['name']]."\n" : '').'［'.$stationInfo['line_name'].'］'.($stationInfo['wc_position'] ?? '无数据，该站可能无卫生间');
 	}
