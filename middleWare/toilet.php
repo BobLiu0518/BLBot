4f0ec82db6215c28a4684c2fdc34dc3f6a0caedb
BobLiu <bobliu@bobliu.tech>
2024-04-22 17:11:40 +0800

    Update #toilet: Fix bug

--- middleWare/toilet.php
+++ middleWare/toilet.php
@@ -6,7 +6,6 @@ if(preg_match('/(洗手间|卫生间|厕所)$/', $Message)){
     $Message = preg_replace('/(洗手间|卫生间|厕所)/', '', $Message);
     if(mb_strlen($Message, 'UTF-8') < 12){
         $Command[0] = 'middleWare/toilet';
-        if(preg_match('/(黄陂南路|新天地)/', $Message, $match)) $Message = '一大会址·'.$match[1];
         loadModule('toilet');
     }
     leave();
