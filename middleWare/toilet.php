ac363cc5d92cf0a068202dbb67e77387475539e9
BobLiu <bobliu@bobliu.tech>
2024-04-29 18:52:05 +0800

    Update #toilet: Optimize output

--- middleWare/toilet.php
+++ middleWare/toilet.php
@@ -2,8 +2,8 @@
 
 global $Message, $Command;
 
-if(preg_match('/(洗手间|卫生间|厕所)$/', $Message)){
-    $Message = preg_replace('/(洗手间|卫生间|厕所)/', '', $Message);
+if(preg_match('/^(.+)(洗手间|卫生间|厕所|洗手間|衛生間|廁所)$/', $Message, $match)){
+    $Message = trim($match[1]);
     if(mb_strlen($Message, 'UTF-8') < 12){
         $Command[0] = 'middleWare/toilet';
         loadModule('toilet');
