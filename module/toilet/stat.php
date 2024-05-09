f6dc73df367854cfdc98d98accfffaa9fcf6dc86
BobLiu <bobliu@bobliu.tech>
2024-05-09 10:55:16 +0800

    Update #toilet.stat: Optimize output && Support custom threshold

--- module/toilet/stat.php
+++ module/toilet/stat.php
@@ -2,14 +2,19 @@
 
 $data = json_decode(getData('toilet/data.json'), true);
 $stations = [];
+$threshold = max(nextArg() ?? 5, 3);
 foreach($data as $company){
 	foreach($company as $station => $toilet){
 		$stations[] = $station;
 	}
 }
 $count = array_count_values($stations);
-$count = array_filter($count, function($value){ return $value > 2; });
 arsort($count);
-replyAndLeave(var_export($count, true));
+
+$reply = [];
+foreach($count as $station => $times){
+	if($times >= $threshold) $reply[] = '['.$times.'] '.$station;
+}
+replyAndLeave(implode("\n", count($reply) ? $reply : ['无']));
 
 ?>
