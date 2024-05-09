e8787531b9fa47e655b82b266f7a844226a3b9e1
BobLiu <bobliu@bobliu.tech>
2024-05-09 11:16:01 +0800

    Update #toilet.cities: Sending table image if no arguments specified

--- module/toilet/cities.php
+++ module/toilet/cities.php
@@ -1,6 +1,12 @@
 <?php
 
-$data = json_decode(getData('toilet/data.json'), true);
-replyAndLeave("当前支持查询的城市：\n".implode('、', array_keys($data)).'（按接入顺序排序，更多城市接入中…）');
+global $Queue;
+$img = trim(sendImg(getImg('toilet.png')));
+if($img && !nextArg()){
+	$Queue[]= replyMessage($img);
+}else{
+	$data = json_decode(getData('toilet/data.json'), true);
+	$Queue[]= replyMessage("当前支持查询的城市：\n".implode('、', array_keys($data)).'（按接入顺序排序）');
+}
 
 ?>
