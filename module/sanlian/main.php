adc37f7f9f61a7c386e005df6fb8d8edf3a111e7
BobLiu <bobliu@bobliu.tech>
2024-05-12 17:53:23 +0800

    Update #sanlian: Optimize output

--- module/sanlian/main.php
+++ module/sanlian/main.php
@@ -1,10 +1,17 @@
 <?php
 
-global $Message;
+global $Message, $Queue;
 if(!$Message) $Message = '三连';
 
 loadModule('jrrp');
 loadModule('checkin');
 loadModule('mrrp');
+
+$reply = [];
+$messages = array_splice($Queue, -3, 3);
+foreach($messages as $message){
+	$reply[] = preg_replace('/\[CQ:reply,id=.+?\]/', '', $message->msg);
+}
+$Queue[] = replyMessage(implode("\n══════════\n", $reply));
 
 ?>
