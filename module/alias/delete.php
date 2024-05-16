2389f6bf6eff35835f1d27c079e4ee0a7dea1fc7
BobLiu <bobliu@bobliu.tech>
2024-05-16 14:00:00 +0800

    Update #alias.del: Optimize output

--- module/alias/delete.php
+++ module/alias/delete.php
@@ -5,10 +5,15 @@
 	$alias = nextArg();
 	if($alias !== NULL){
 		$alias = ltrim($alias, '#');
-		delAlias($Event['user_id'], $alias);
-		$Queue[]= replyMessage('删除别名 #'.$alias.' 成功～');
-		$Queue[]= sendMaster($Event['user_id'].' 删除了别名 #'.$alias);
-	}else
+		if(chkAlias($Event['user_id'])[$alias]){
+			delAlias($Event['user_id'], $alias);
+			$Queue[]= replyMessage('删除别名 #'.$alias.' 成功～');
+			$Queue[]= sendMaster($Event['user_id'].' 删除了别名 #'.$alias);
+		}else{
+			$Queue[]= replyMessage('你没有设置过名为 #'.$alias.' 的别名哦…');
+		}
+	}else{
 		$Queue[]= replyMessage('不知道你打算删什么别名呢…');
+	}
 
 ?>
