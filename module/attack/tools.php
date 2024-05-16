6534784b91056ab0844deff64dc0f210e7e61377
BobLiu <bobliu@bobliu.tech>
2024-05-16 14:04:18 +0800

    Update #attack: Fix bugs

--- module/attack/tools.php
+++ module/attack/tools.php
@@ -46,12 +46,12 @@ function attack($from, $target, $atTarget, $dreaming = false){
 			$prisonRate = pow(2, $data['count']['times']);
 			$success = rand(1, 100) <= $successRate;
 			$prison = rand(1, 100) <= $prisonRate;
-			$getMoney = ceil((getLvl($from) - getLvl($target) + 10) * (getRp($from, time()) - getRp($target, time()) + 100) * rand(100 * $magnification, 1000 * $magnification) / 200 + 1);
+			$getMoney = ceil((getLvl($from) - getLvl($target) + 10) * (getRp($from, time()) - getRp($target, time()) + 100) * rand(ceil(100 * $magnification), ceil(1000 * $magnification)) / 200 + 1);
 			if(getCredit($target) - 10000 <= $getMoney) $getMoney = getCredit($target) - 9999;
 			if(getCredit($target) < 10000) $success = false;
 
 			if($success && $prison){
-				$fine = ceil(sqrt($getMoney) * 10) + 500 * $magnification;
+				$fine = ceil(sqrt($getMoney) * 10 + 500 * $magnification);
 				decCredit($from, $fine, true);
 				$data['status'] = 'imprisoned';
 				$data['end'] = date('Ymd', time() + 86400 * 2);
