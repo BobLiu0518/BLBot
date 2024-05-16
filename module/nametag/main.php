5b346551a8dd961b16c7c43f8a5659ca3b60a692
BobLiu <bobliu@bobliu.tech>
2024-05-16 16:58:57 +0800

    Add function #nametag back

---
+++ module/nametag/main.php
@@ -0,0 +1,29 @@
+<?php
+
+global $Event, $CQ, $Command;
+
+$reply = [];
+$tag = implode(' ', array_splice($Command, 1));
+
+if(!fromGroup()){
+	replyAndLeave('设置群头衔功能仅能在群聊中使用哦…');
+}else if(preg_match('/\[CQ:/', $tag)){
+	replyAndLeave('不支持使用 QQ 表情等特殊内容作为头衔哦…');
+}else if($CQ->getGroupMemberInfo($Event['group_id'], config('bot'), true)->role != 'owner'){
+	replyAndLeave('Bot 似乎不是群主，没法设置群头衔呢…');
+}
+
+if(strlen($tag) > 18){
+	$reply[] = '头衔过长，可能会被截断为「'.mb_convert_encoding(substr($tag, 0, 18), 'UTF-8').'」';
+}
+
+$CQ->setGroupSpecialTitle($Event['group_id'], $Event['user_id'], $tag);
+if($tag){
+	$reply[] = '设置群头衔为「'.$tag.'」成功～';
+}else{
+	$reply[] = '清除群头衔成功～';
+}
+
+replyAndLeave(implode("\n", $reply));
+
+?>
