04efbf1d491a6157c9265bb0c0bc3f3e4ab5b916
BobLiu <bobliu@bobliu.tech>
2024-05-16 13:57:51 +0800

    Update #rh.nickname: Optimize output

--- module/rh/nickname.php
+++ module/rh/nickname.php
@@ -6,8 +6,12 @@ global $Event;
 $char = nextArg();
 
 if(!$char){
-	delData('rh/user/'.$Event['user_id']);
-	replyAndLeave('已删除昵称~');
+	if($data = getData('rh/user/'.$Event['user_id'])){
+		delData('rh/user/'.$Event['user_id']);
+		replyAndLeave('已删除昵称「'.json_decode($data, true)['nickname'].'」~');
+	}else{
+		replyAndLeave('你还没有设置昵称哦，使用 #rh.nickname <昵称> 即可设置~');
+	}
 }else{
 	$code = mb_ord($char, 'UTF-8');
 	if(mb_strlen($char, 'UTF-8') == 1 && ($code >= 0x3400 && $code <= 0x9FBF || $code >= 0xF900 && $code <= 0xFAFF || $code >= 0x20000 && $code <= 0x2FA1F)){
