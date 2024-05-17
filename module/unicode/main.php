7eb29ab70f721c43e67ec223d1b10c4e362b1f8e
BobLiu <bobliu@bobliu.tech>
2024-05-17 12:22:53 +0800

    New Feature: #unicode

---
+++ module/unicode/main.php
@@ -0,0 +1,13 @@
+<?php
+
+global $Command;
+requireLvl(6);
+
+if(count($Command) == 1){
+	replyAndLeave('不知道你想查询什么呢…');
+}else{
+	$char = trim(implode(' ', array_splice($Command, 1)).$Text);
+}
+replyAndLeave(trim(shell_exec('../module/unicode/unidata.py '.escapeshellarg($char))));
+
+?>
