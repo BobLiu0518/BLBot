c7eca4435cbcbc1f16dc62d8937a76e4c3010fea
BobLiu <bobliu@bobliu.tech>
2024-05-29 09:49:14 +0800

    Fix bug

--- public/messageProcessor.php
+++ public/messageProcessor.php
@@ -8,7 +8,7 @@ if(false===$length){
 
 if(preg_match('/^('.config('prefix', '#').')/', $message, $prefix)
     || preg_match('/^('.config('prefix2', '.').')/', $message, $prefix) && config('enablePrefix2', false)){
-    $Command = parseCommand(substr($message, strlen($prefix[1])-1, $length));
+    $Command = parseCommand(mb_substr($message, mb_strlen($prefix[1])-1, $length));
     $Text = substr($message, $length+1);
     $module = substr(nextArg(), strlen($prefix[1]));
     try{
