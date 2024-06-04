40be2fee178ecf315df78816d2eca142b67bdd8f
BobLiu <bobliu@bobliu.tech>
2024-06-04 20:06:32 +0800

    Update #choose: Reduce misidentify

--- middleWare/choose.php
+++ middleWare/choose.php
@@ -10,7 +10,7 @@ function removeChar($str){
 
 global $Event, $Command, $Queue;
 
-if(mb_strlen($Event['message']) <= 32 && !preg_match('/(但|估计|大概|可能|就|不知道|也许|还是说|来着|只是|真要看|不好说|不管|到底|指的是|关键是|什么的)/', $Event['message']) && preg_match("/(?:选|是)(.+?)还是(.+)/", $Event['message'], $result) && strpos($Event['message'], '你') === false && strpos($Event['message'], '我') === false){
+if(mb_strlen($Event['message']) <= 32 && !preg_match('/(但|估计|大概|可能|就|不知道|也许|还是说|来着|只是|真要看|不好说|不管|到底|指的是|关键是|什么)/', $Event['message']) && preg_match("/(?:选|是)(.+?)还是(.+)/", $Event['message'], $result) && strpos($Event['message'], '你') === false && strpos($Event['message'], '我') === false){
 	$Command = array('middleware-choose', removeChar($result[1]));
 	foreach(explode('还是', $result[2]) as $thing)
 		if(removeChar($thing))
