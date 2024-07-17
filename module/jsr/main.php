be66e908cba386f51899cad7bf63629ce608358c
BobLiu <bobliu@bobliu.tech>
2024-07-17 15:39:15 +0800

    Update #jsr: Optimize output

--- module/jsr/main.php
+++ module/jsr/main.php
@@ -5,13 +5,15 @@ use zjkal\ChinaHoliday;
 
 $search = nextArg();
 if(!$search) replyAndLeave('不知道你要查询什么呢…（可以查询金山铁路车次或车站）');
-if(preg_match('/^S?(\d+)$/i', $search, $match)){
+if(preg_match('/^(G|D|C|Z|T|K|Y|L|X|N)(\d+)$/i', $search, $match)){
+	replyAndLeave('本指令仅用于查询金山铁路车站和车次，如欲查询其他车次，请使用 '.Config('prefix').'cr 指令。');
+}else if(preg_match('/^S?(\d+)$/i', $search, $match)){
 	$code = 'S'.$match[1];
 	$trains = json_decode(getData('jsr/train.json'), true);
 	if(!$trains[$code]) replyAndLeave('［金山铁路］车次 '.$code.' 不存在或已停开…');
 	$train = $trains[$code];
-	$reply = '［金山铁路］'.$train['code'].'次';
-	$reply .= "\n  ".$train['from'].' → '.$train['to'].' '.preg_replace('/ \(.+\)$/', '', $train['type']);
+	$reply = $train['code'].'次 金山铁路';
+	$reply .= "\n".$train['from'].' → '.$train['to'].' '.preg_replace('/ \(.+\)$/', '', $train['type']);
 	if($train['dates'] == 'weekdays'){
 		$reply .= "\n* 仅工作日开行";
 	}else if($train['dates'] == 'weekends'){
@@ -40,9 +42,9 @@ if(preg_match('/^S?(\d+)$/i', $search, $match)){
 		if(strnatcmp(date('H:i', $time), $train['time']) < 0) break;
 	}
 	$result = array_splice($trains, $i, 10);
-	$reply = '［金山铁路］'.$station.'站 '.date('n月j日', $time)."\n  ".date('H:i', $time).' 起最近 10 次列车：';
+	$reply = '［金山铁路］'.$station."站\n  ".date('n月j日 H:i', $time).' 起最近 10 次列车：';
 	foreach($result as $train){
-		$reply .= "\n".$train['time'].' '.$train['code'];
+		$reply .= "\n· ".$train['time'].' '.$train['code'];
 		$reply .= $train['to'] == $station ? ' [终到]' : ' 往'.$train['to'];
 		$reply .= ' '.$train['type'];
 	}
