24d485051e418760da8921742441db6c625d7303
BobLiu <bobliu@bobliu.tech>
2024-07-25 00:00:20 +0800

    Update #a2b #b2a: New algo

--- module/bv/decode.php
+++ module/bv/decode.php
@@ -1,22 +1,12 @@
 <?php
 
 global $Queue;
+loadModule('bv.tools');
 
 $bv = nextArg();
-if(strpos($bv, 'BV') !== 0)replyAndLeave('请输入有效的BV号哦～');
-
-$table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
-$tr = array();
-for($i = 0; $i < 58; $i += 1)
-        $tr[$table[$i]] = $i;
-$s = array(11, 10, 3, 8, 4, 6);
-$xorVal = 177451812;
-$addVal = 8728348608;
-$r = 0;
-for($i = 0; $i < 6; $i += 1)
-	$r += $tr[$bv[$s[$i]]] * pow(58, $i);
-$av = ($r - $addVal) ^ $xorVal;
-
-$Queue[]= replyMessage('av'.$av);
+if(!preg_match('/^BV[a-km-zA-HJ-NP-Z1-9]+$/', $bv)){
+	replyAndLeave('请输入有效的BV号哦～');
+}
+$Queue[]= replyMessage(B23::BV2AV($bv));
 
 ?>
--- module/bv/encode.php
+++ module/bv/encode.php
@@ -1,20 +1,15 @@
 <?php
 
 global $Queue;
+loadModule('bv.tools');
 
-$av = str_replace('av', '', str_replace('AV', '', nextArg()));
-if(!is_numeric($av))replyAndLeave('请输入有效的av号哦～');
-
-$table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
-$s = array(11, 10, 3, 8, 4, 6);
-$xorVal = 177451812;
-$addVal = 8728348608;
-$x = ($av ^ $xorVal) + $addVal;
-$r = array('B', 'V', '1', ' ', ' ', '4', ' ', '1', ' ', '7', ' ', ' ');
-for($i = 0; $i < 6; $i += 1)
-	$r[$s[$i]] = $table[ceil($x / pow(58, $i) % 58)];
-$bv = implode($r);
-
-$Queue[]= replyMessage($bv);
+$av = nextArg();
+if(preg_match('/^\d+$/', $av)){
+	$av = 'av'.$av;
+}
+if(!preg_match('/^(av)?\d+$/i', $av)){
+	replyAndLeave('请输入有效的av号哦～');
+}
+$Queue[]= replyMessage(B23::AV2BV($av));
 
 ?>
