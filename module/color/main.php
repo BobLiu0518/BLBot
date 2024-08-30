b5b0f0036ac3d97c4a673d6c4a9edb2c6b89ff2f
BobLiu <bobliu@bobliu.tech>
2024-08-30 19:29:09 +0800

    Update #color: Optimize

--- module/color/main.php
+++ module/color/main.php
@@ -3,19 +3,26 @@
 requireLvl(1);
 
 $color = nextArg();
-if(!$color) $color = '#'.strtoupper(dechex(rand(0, 255)).dechex(rand(0, 255)).dechex(rand(0, 255)));
+if(!$color){
+    $color = '#'.substr('000000'.dechex(rand(0, pow(2, 24) - 1)), -6);
+}else if(preg_match('/^[0-9a-f]{6}$/i', $color)){
+    $color = '#'.$color;
+}
 
+$pixel = new ImagickPixel($color);
 $image = new Imagick();
-$image->newImage(640, 360, $color);
+$image->newImage(640, 360, $pixel);
 $image->setImageFormat('png');
 $draw = new ImagickDraw();
 $draw->setGravity(Imagick::GRAVITY_NORTHWEST);
 $draw->setFont(getFontPath('consolab.ttf'));
 $draw->setFontSize(56);
-$size = $image->queryFontMetrics($draw, $color);
-$draw->setFillColor((new ColorCompare\Color($color))->getLab()['L'] > 70 ? '#000000' : '#FFFFFF');
-$draw->annotation((640 - $size['textWidth']) / 2, (360 - $size['textHeight']) / 2, $color);
-
+$rgb = $pixel->getColor();
+unset($rgb['a']);
+$colorCompare = new ColorCompare\Color($rgb);
+$hex = strtoupper($colorCompare->getHex());
+$size = $image->queryFontMetrics($draw, $hex);
+$draw->setFillColor($colorCompare->getLab()['L'] > 70 ? '#000000' : '#FFFFFF');
+$draw->annotation((640 - $size['textWidth']) / 2, (360 - $size['textHeight']) / 2, $hex);
 $image->drawImage($draw);
 replyAndLeave(sendImg($image->getImageBlob()));
-
