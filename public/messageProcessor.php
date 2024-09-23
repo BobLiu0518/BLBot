45cb63d59da9315a70ec85c9793836a0674b3d34
BobLiu <bobliu@bobliu.tech>
2024-09-23 21:40:57 +0800

    Add support to

--- public/messageProcessor.php
+++ public/messageProcessor.php
@@ -1,31 +1,33 @@
 <?php
 
-$message = trim(preg_replace('/(\[CQ:at,qq='.config('bot').'\]|\[CQ:reply,id=\d+\])/', '', $Event['message']));
+$message = trim(preg_replace('/\[CQ:at,qq='.config('bot').'(,name=.+?)?\]/', '', $Event['message']));
+if(preg_match('/\[CQ:reply,id=(-?\d+?)\]/', $message, $matches)) {
+    $Referer = $matches[1];
+    $message = preg_replace('/\[CQ:reply,id=(-?\d+?)\]/', '', $message);
+}
+
 $length = strpos($message, PHP_EOL);
-if(false===$length){
+if(false === $length) {
     $length = strlen($message);
 }
 
 if(preg_match('/^('.config('prefix', '#').')/', $message, $prefix)
-    || preg_match('/^('.config('prefix2', '.').')/', $message, $prefix) && config('enablePrefix2', false)){
-    $Command = parseCommand(mb_substr($message, mb_strlen($prefix[1])-1, $length));
-    $Text = substr($message, $length+1);
+    || preg_match('/^('.config('prefix2', '.').')/', $message, $prefix) && config('enablePrefix2', false)) {
+    $Command = parseCommand(mb_substr($message, mb_strlen($prefix[1]) - 1, $length));
+    $Text = substr($message, $length + 1);
     $module = substr(nextArg(), strlen($prefix[1]));
-    try{
-        if(config('alias',false) == true
-            && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'),true)[$module]){
+    try {
+        if(config('alias', false) == true
+            && $alias = json_decode(getData('alias/'.$Event['user_id'].'.json'), true)[$module]) {
             loadModule($alias);
-        }
-        else
+        } else
             loadModule($module);
-    }catch(\Exception $e){
+    } catch (\Exception $e) {
         throw $e;
     }
-}else{ //不是命令
+} else { //不是命令
     $Message = $message;
     $Command = parseCommand(substr($message, 0, $length));
-    $Text = substr($message, $length+1);
+    $Text = substr($message, $length + 1);
     require('../middleWare/Chain.php');
 }
-
-?>
