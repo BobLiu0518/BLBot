297a9294f737f1a92edc2e3da95b8d4e894cd686
BobLiu <bobliu@bobliu.tech>
2024-09-28 11:59:01 +0800

    Update #toilet: Fix Shanghai data

--- module/toilet/update/shanghai.php
+++ module/toilet/update/shanghai.php
@@ -27,20 +27,21 @@ foreach($stations as $station) {
     // Load data
     $stationInfo = json_decode(file_get_contents($stationInfoApi.$station['key']), true)[0];
     $stationName = $station['value'];
-    $toilets = json_decode($stationInfo['toilet_position'], true)['toilet'];
+    $toilets = json_decode($stationInfo['toilet_position'] ?? '', true)['toilet'];
+
+    // Handle redundance space
+    $stationName = preg_replace('/\s+$/u', '', $stationName);
 
     // Skip fake stations
     if(preg_match('/^[内外]圈(\(宜山路\))?/u', $stationName)) continue;
-
-    // Handle Pudian Road station conflict
-    if($stationName == '浦电路') $stationName = '浦电路（4号线）';
-    else if($stationName == '浦电路 ') $stationName = '浦电路（6号线）';
 
     // Remove redundant "站"
     else if(preg_match('/^外高桥保税区[南北]站$/u', $stationName)) $stationName = preg_replace('/站$/', '', $stationName);
 
     // Match data
-    $toiletInfo['shanghai'][$stationName] = ['toilets' => []];
+    if(!array_key_exists($stationName, $toiletInfo['shanghai'])) {
+        $toiletInfo['shanghai'][$stationName] = ['toilets' => []];
+    }
     foreach($toilets as $toilet) {
         if($toilet['lineno'] == 41) {
             $lineName = '浦江线';
@@ -48,10 +49,13 @@ foreach($stations as $station) {
             $lineName = $toilet['lineno'].'号线';
         }
 
-        $toiletInfo['shanghai'][$stationName]['toilets'][] = [
+        $info = [
             'title' => $lineName,
             'content' => $toilet['description'],
         ];
+        if(!in_array($info, $toiletInfo['shanghai'][$stationName]['toilets'])){
+            $toiletInfo['shanghai'][$stationName]['toilets'][] = $info;
+        }
     }
 }
 
@@ -65,12 +69,21 @@ foreach($lines as $line) {
     $citiesMeta['shanghai']['color'][$lineName] = $line['color'];
 }
 
-// Handle Pudian Road station conflict
-$toiletInfo['shanghai']['浦电路'] = ['redirect' => ['浦电路（4号线）', '浦电路（6号线）']];
-
 // Set aliases
+$toiletInfo['shanghai']['济阳路'] = ['redirect' => ['东方体育中心']];
+$toiletInfo['shanghai']['船厂路'] = ['redirect' => ['龙华中路']];
+$toiletInfo['shanghai']['周家渡'] = ['redirect' => ['中华艺术宫']];
+$toiletInfo['shanghai']['航天博物馆'] = ['redirect' => ['沈杜公路']];
+$toiletInfo['shanghai']['卢浦大桥'] = ['redirect' => ['世博会博物馆']];
 $toiletInfo['shanghai']['黄陂南路'] = ['redirect' => ['一大会址·黄陂南路']];
 $toiletInfo['shanghai']['新天地'] = ['redirect' => ['一大会址·新天地']];
+$toiletInfo['shanghai']['徐泾东'] = ['redirect' => ['国家会展中心']];
+$toiletInfo['shanghai']['东昌路'] = ['redirect' => ['浦东南路']];
+$toiletInfo['shanghai']['浦东国际机场'] = ['redirect' => ['浦东1号2号航站楼']];
+$toiletInfo['shanghai']['浦电路']['redirect'] = ['向城路'];
+$toiletInfo['shanghai']['松江南站'] = ['redirect' => ['上海松江站']];
+$toiletInfo['shanghai']['华泾西'] = ['redirect' => ['景洪路']];
+$toiletInfo['shanghai']['诸光路'] = ['redirect' => ['国家会展中心']];
 
 // Save data
 setData('toilet/toiletInfo.json', json_encode($toiletInfo));
