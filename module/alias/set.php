58c51b98080baf96ba4e31541a2f957f5a1d8515
BobLiu <bobliu@bobliu.tech>
2024-11-06 10:36:57 +0800

    fix: Fix #alias: Optimize command parsing

--- module/alias/set.php
+++ module/alias/set.php
@@ -1,30 +1,38 @@
 <?php
 
-	global $Queue, $Event;
-	loadModule('alias.tools');
-	loadModule('credit.tools');
-	requireLvl(2);
-	$alias = nextArg();
-	$command = nextArg();
-	if($alias !== NULL && $command !== NULL){
-		$alias = ltrim($alias, '#');
-		$command = ltrim($command, '#');
-		if(strpos(preg_replace('/\\[CQ:(?:emoji|face),id=\\d*?\\]/', '啊', $alias), '[CQ:') !== false){
-			replyAndLeave('别名含有不合规内容…');
-		}
-		if(preg_match('/^alias(\.(check|clear|delete|del|set))?$/', $alias)){
-			replyAndLeave('不允许设置别名相关指令为别名哦~');
-		}
-		if(!checkModule($command)){
-			replyAndLeave('原名指令 #'.$command.' 不存在…');
-		}
-		decCredit($Event['user_id'], 1800);
-		setAlias($Event['user_id'], $alias, $command);
-		$Queue[]= replyMessage('设置 #'.$alias.' 为 #'.$command." 的别名成功，已收取 1800 金币！\n请注意 别用违法违规词汇做别名哦\n注：别名仅对自己生效哦～");
-		$Queue[]= sendMaster($Event['user_id'].' 设置了 #'.$alias.' 作为 #'.$command.' 的别名');
-	}else if(strpos($Text, "[CQ:") !== false)
-		$Queue[]= replyMessage("非法内容…");
-	else
-		$Queue[]= replyMessage('不知道你想设置什么呢…');
+function parseCommandName($name) {
+	global $Config;
+	if($Config['enablePrefix2']) {
+		$pattern = '/^('.$Config['prefix'].'|'.$Config['prefix2'].')/u';
+	} else {
+		$pattern = '/^'.$Config['prefix'].'/u';
+	}
+	return strtolower(preg_replace($pattern, '', $name));
+}
 
-?>
+global $Queue, $Event;
+loadModule('alias.tools');
+loadModule('credit.tools');
+requireLvl(2);
+$alias = nextArg();
+$command = nextArg();
+if($alias !== null && $command !== null) {
+	$alias = parseCommandName($alias);
+	$command = parseCommandName($command);
+	if(strpos(preg_replace('/\\[CQ:(?:emoji|face),id=\\d*?\\]/', '啊', $alias), '[CQ:') !== false) {
+		replyAndLeave('别名含有不合规内容…');
+	}
+	if(preg_match('/^alias(\.(check|clear|delete|del|set))?$/', $alias)) {
+		replyAndLeave('不允许设置别名相关指令为别名哦~');
+	}
+	if(!checkModule($command)) {
+		replyAndLeave('原名指令 #'.$command.' 不存在…');
+	}
+	decCredit($Event['user_id'], 1800);
+	setAlias($Event['user_id'], $alias, $command);
+	$Queue[] = replyMessage('设置 #'.$alias.' 为 #'.$command." 的别名成功，已收取 1800 金币！\n请注意 别用违法违规词汇做别名哦\n注：别名仅对自己生效哦～");
+	$Queue[] = sendMaster($Event['user_id'].' 设置了 #'.$alias.' 作为 #'.$command.' 的别名');
+} else if(strpos($Text, "[CQ:") !== false)
+	$Queue[] = replyMessage("非法内容…");
+else
+	$Queue[] = replyMessage('不知道你想设置什么呢…');
