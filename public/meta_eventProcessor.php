e9c00967cfbe4aa6c0ee6624d8d309bf2e1d8e5e
BobLiu <bobliu@bobliu.tech>
2024-11-06 11:08:50 +0800

    style: Remove junk

--- public/meta_eventProcessor.php
+++ public/meta_eventProcessor.php
@@ -2,8 +2,6 @@
 
 global $Queue, $CQ, $Config;
 
-$cd = 1 * 60;
-
 if(intval(date('s') < 5) && intval(date('i')) == 0) {
 	$CQ->setGroupName(intval($Config['devgroup']), $Config['devgroupName'].' @'.date('H').'点了！');
 }
