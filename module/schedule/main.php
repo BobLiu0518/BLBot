2e393e282acb4c79bab55edd56bc35b476e0cf3a
BobLiu <bobliu@bobliu.tech>
2024-11-19 12:53:19 +0800

    style: Rename #schedule.check => #schedule.everyone

--- module/schedule/main.php
+++ module/schedule/main.php
@@ -3,7 +3,7 @@
 global $Command;
 
 if(count($Command) - 1 == 0) {
-    loadModule('schedule.check');
+    loadModule('schedule.everyone');
 } else if(count($Command) - 1 == 1) {
     loadModule('schedule.set');
 }
