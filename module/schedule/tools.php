061a740a610acb047501d2768c1e71531ea0d3e5
BobLiu <bobliu@bobliu.tech>
2024-11-20 17:13:52 +0800

    fix: Update #schedule: Fix timezone

--- module/schedule/tools.php
+++ module/schedule/tools.php
@@ -1,11 +1,14 @@
 <?php
 
 function getWeek($semesterStart, $current) {
+    $timezone = new DateTimeZone('Asia/Shanghai');
     $semesterStart = new DateTime('@'.$semesterStart);
+    $semesterStart->setTimezone($timezone);
     $semesterStart->modify('Monday this week');
     $currentWeekStart = new DateTime('@'.$current);
+    $semesterStart->setTimezone($timezone);
     $currentWeekStart->modify('Monday this week');
-    return $semesterStart->diff($currentWeekStart)->days / 7;
+    return $semesterStart->diff($currentWeekStart)->days / 7 + 1;
 }
 
 function getCourses($user_id, $date) {
