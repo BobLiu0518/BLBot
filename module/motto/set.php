49ff2d0e92e50e18ca8eac14e498ec3cda43ebd1
BobLiu <bobliu@bobliu.tech>
2024-11-23 19:59:44 +0800

    feat: update #motto: optimize limitation

--- module/motto/set.php
+++ module/motto/set.php
@@ -3,7 +3,7 @@
 global $Event;
 loadModule('motto.tools');
 
-$motto = nextArg();
+$motto = nextArg(true);
 if(!$motto) {
     replyAndLeave('不知道你要设置什么作为个性签名呢…');
 }
@@ -13,8 +13,8 @@ if(preg_match('/[\x{0000}-\x{001F}\x{007F}-\x{009F}]/u', $motto)) {
 if(preg_match('/\[CQ:/', $motto)) {
     replyAndLeave('个性签名中不能包含特殊内容哦…');
 }
-if(preg_match('/\s/', $motto)) {
-    replyAndLeave('个性签名中不能包含空白字符哦…');
+if(preg_match('/\r|\n/', $motto)) {
+    replyAndLeave('个性签名中不能包含换行哦…');
 }
 if(mb_strlen($motto) > 32) {
     replyAndLeave('个性签名太长了哦…');
