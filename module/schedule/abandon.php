7688b1776f8e633b8dad400a5056fae4942bdff7
BobLiu <bobliu@bobliu.tech>
2024-11-24 10:59:20 +0800

    fix: fix #abandon: add schedule not imported prompt

--- module/schedule/abandon.php
+++ module/schedule/abandon.php
@@ -4,6 +4,9 @@ global $Event;
 requireLvl(1);
 loadModule('schedule.tools');
 
+if(!getScheduleData($Event['user_id'])) {
+    replyAndLeave('尚未导入课表数据，无法设置翘课哦，请先用 #schedule.set 导入课表数据～');
+}
 $abandoned = !isAbandoned($Event['user_id']);
 setAbandoned($Event['user_id'], $abandoned);
 replyAndLeave($abandoned ? '已设置今日翘课～' : '已取消今日翘课～');
