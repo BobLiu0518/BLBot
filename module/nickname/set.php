cfabcfb696c803677826821341d84827d59d038d
BobLiu <bobliu@bobliu.tech>
2024-11-25 15:56:57 +0800

    feat: update #nickname: optimize limitation

--- module/nickname/set.php
+++ module/nickname/set.php
@@ -2,7 +2,7 @@
 
 global $Event;
 
-$nickname = nextArg();
+$nickname = nextArg(true);
 if(!$nickname) {
     replyAndLeave('不知道你要设置什么作为昵称呢…');
 }
@@ -12,8 +12,8 @@ if(preg_match('/[\x{0000}-\x{001F}\x{007F}-\x{009F}]/u', $nickname)) {
 if(preg_match('/\[CQ:/', $nickname)) {
     replyAndLeave('昵称中不能包含特殊内容哦…');
 }
-if(preg_match('/\s/', $nickname)) {
-    replyAndLeave('昵称中不能包含空白字符哦…');
+if(preg_match('/\r|\n/', $nickname)) {
+    replyAndLeave('昵称中不能包含换行哦…');
 }
 if(mb_strlen($nickname) > 20) {
     replyAndLeave('昵称太长了哦…');
