b37a498a156f12a5d67853f5a1c6ea681a7cae71
BobLiu <bobliu@bobliu.tech>
2024-11-30 19:57:58 +0800

    feat: update #schedule.check: add date recognize

--- module/schedule/check.php
+++ module/schedule/check.php
@@ -4,20 +4,34 @@ requireLvl(1);
 loadModule('schedule.tools');
 
 global $Event, $CQ;
-$target = nextArg() ?? $Event['user_id'];
-if(!is_numeric($target)) $target = parseQQ($target);
-if(!is_numeric($target)) $target = $Event['user_id'];
+
+$arg = nextArg();
+if(preg_match('/^(?:\[CQ:at,qq=)?(\d+)(?:])?$/', $arg, $matches)) {
+    $target = intval($matches[1]);
+    $arg = nextArg(true);
+} else {
+    $target = $Event['user_id'];
+    $arg .= trim(' '.nextArg(true));
+}
+if($arg) {
+    $time = strtotime($arg);
+    $date = date('Y/m/d', $time);
+} else {
+    $time = time();
+    $date = '今日';
+}
+
 $user = $CQ->getGroupMemberInfo($Event['group_id'], $target);
 $nickname = $user->card ?? $user->nickname ?? replyAndLeave("{$target} 不在本群哦…");
 
-$courses = getCourses($target, time());
+$courses = getCourses($target, $time);
 if($courses === false) {
     replyAndLeave($nickname.' 未配置课程表哦…');
 } else if(!count($courses)) {
-    replyAndLeave($nickname.' 今日无课～');
+    replyAndLeave($nickname." {$date} 无课～");
 }
 
-$result = [$nickname.' 今日课程：'];
+$result = [$nickname." {$date} 课程："];
 foreach($courses as $course) {
     $result[] = "{$course['startTime']}~{$course['endTime']} {$course['name']}";
 }
