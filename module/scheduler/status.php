28701cc3adf5012092b936b541c0bd5d3f3202e8
BobLiu <bobliu@bobliu.tech>
2024-12-01 20:24:46 +0800

    fix: fix misspelling

--- module/scheduler/status.php
+++ module/scheduler/status.php
@@ -8,7 +8,7 @@ $data = $db->find();
 $reply = [];
 
 foreach($data as $scheduler) {
-    $lastExcuted = date('Y/m/d H:i:s', $scheduler['lastExcuted']);
+    $lastExcuted = date('Y/m/d H:i:s', $scheduler['lastExecute']);
     $reply[] = "[{$scheduler['name']}] Last executed:\n  {$lastExcuted}";
 }
 replyAndLeave(implode("\n", $reply));
