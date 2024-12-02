324f480205a493141f7d34cb4baae89307884ae1
BobLiu <bobliu@bobliu.tech>
2024-12-02 23:41:39 +0800

    feat: update #ddl.finish: finish several tasks in one command

--- module/ddl/finish.php
+++ module/ddl/finish.php
@@ -3,10 +3,12 @@
 global $Event;
 loadModule('ddl.tools');
 
-$name = nextArg();
-if(!$name) {
+$names = nextArg(true);
+if(!$names) {
     replyAndLeave('不知道你想完成什么呢…');
 }
 
-finishDdl($Event['user_id'], $name);
+foreach(explode(' ', $names) as $name) {
+    finishDdl($Event['user_id'], $name);
+}
 replyAndLeave('恭喜完成任务～');
