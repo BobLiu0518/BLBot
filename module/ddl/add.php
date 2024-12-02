81b1c61e79375305d2ca6d74ec7ef2b387292073
BobLiu <bobliu@bobliu.tech>
2024-12-02 23:12:29 +0800

    fix: update #ddl.add: fix bug

--- module/ddl/add.php
+++ module/ddl/add.php
@@ -9,14 +9,14 @@ $time = nextArg(true);
 if(!$name || !$time) {
     replyAndLeave('不知道你想设置什么呢…');
 }
-$time = strtotime($time);
-if(!$time) {
+$ddl = strtotime($time);
+if(!$ddl) {
     replyAndLeave("无法识别的时间：{$time}");
 }
 if(mb_strlen($name) > 10) {
     replyAndLeave('名称太长了哦，精简一下吧～');
 }
 
-setDdl($Event['user_id'], $name, $time);
-$time = date('Y/m/d', $time);
-replyAndLeave("设置任务 {$name} 成功，截止：{$time}");
+setDdl($Event['user_id'], $name, $ddl);
+$time = date('Y/m/d', $ddl);
+replyAndLeave("设置任务 {$name} 成功，截止：{$ddl}");
