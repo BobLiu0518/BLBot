a3c46f35b3196ff8a4c66b328563548799fe66f7
BobLiu <bobliu@bobliu.tech>
2024-12-03 13:06:45 +0800

    docs: update LvBadge: add link to /manual/exp

--- components/LvBadge.vue
+++ components/LvBadge.vue
@@ -1,7 +1,15 @@
 <template>
-    <Badge type="tip" :text="`Lv${props.lv}`" />
+    <Badge type="tip">
+        <a href="/manual/exp" class="no-underline">Lv{{ props.lv }}</a>
+    </Badge>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{ lv: string }>();
 </script>
+
+<style scoped>
+.no-underline {
+    text-decoration: none;
+}
+</style>
