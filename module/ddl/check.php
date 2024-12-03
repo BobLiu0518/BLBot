f6e87b8b2f506828c13cd60c6d660fcd8f2e7863
BobLiu <bobliu@bobliu.tech>
2024-12-03 08:49:00 +0800

    fix: update #ddl: fix bug

--- module/ddl/check.php
+++ module/ddl/check.php
@@ -5,7 +5,7 @@ loadModule('ddl.tools');
 
 $reply = ['你的待办事项：'];
 $ddls = getDdl($Event['user_id']);
-if(!count($ddls)) {
+if(!$ddls) {
     replyAndLeave('暂无未完成的待办事项哦…');
 }
 usort($ddls, function ($a, $b) {
