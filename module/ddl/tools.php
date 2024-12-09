3bfd442d72d21f8f4d0a170d15f47aa634c4b079
BobLiu <bobliu@bobliu.tech>
2024-12-09 14:45:42 +0800

    feat: update #ddl.check: display types

--- module/ddl/tools.php
+++ module/ddl/tools.php
@@ -22,3 +22,41 @@ function finishDdl(int $user_id, string $name) {
         'name' => $name,
     ]);
 }
+
+function classifyDdls($ddls, $timestamp = null) {
+    if(!$ddls) return null;
+    $reply = [];
+    $result = [
+        'expired' => [],
+        'critical' => [],
+        'urgent' => [],
+        'regular' => [],
+    ];
+    foreach($ddls as $ddl) {
+        $remainTime = $ddl['time'] - ($timestamp ?? time());
+        $time = date('Y/m/d', $ddl['time']);
+        $description = "{$time} {$ddl['name']}";
+        if($remainTime < 0) {
+            $result['expired'][] = $description;
+        } else if($remainTime <= 3 * 86400) {
+            $result['critical'][] = $description;
+        } else if($remainTime <= 7 * 86400) {
+            $result['urgent'][] = $description;
+        } else {
+            $result['regular'][] = $description;
+        }
+    }
+    if(count($result['expired'])) {
+        $reply[] = "———— 逾期 ————\n".implode("\n", $result['expired']);
+    }
+    if(count($result['critical'])) {
+        $reply[] = "———— 紧急 ————\n".implode("\n", $result['critical']);
+    }
+    if(count($result['urgent'])) {
+        $reply[] = "———— 优先 ————\n".implode("\n", $result['urgent']);
+    }
+    if(count($result['regular'])) {
+        $reply[] = "———— 其他 ————\n".implode("\n", $result['regular']);
+    }
+    return implode("\n", $reply);
+}
