4e24dfd0290bba3f74a21c01834ed1077840bcbb
BobLiu <bobliu@bobliu.tech>
2024-12-09 14:45:42 +0800

    feat: update #ddl.check: display types

--- module/ddl/check.php
+++ module/ddl/check.php
@@ -3,14 +3,9 @@
 global $Event;
 loadModule('ddl.tools');
 
-$reply = ['你的待办事项：'];
 $ddls = getDdl($Event['user_id']);
 if(!$ddls) {
     replyAndLeave('暂无未完成的待办事项哦…');
 }
-foreach($ddls as $ddl) {
-    $time = date('Y/m/d', $ddl['time']);
-    $reply[] = "{$time} {$ddl['name']}";
-}
 
-replyAndLeave(implode("\n", $reply));
+replyAndLeave(classifyDdls($ddls));
