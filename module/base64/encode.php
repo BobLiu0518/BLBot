cb126d5c6acb9eaa1b5d849358a6f2f41e9541e7
BobLiu <bobliu@bobliu.tech>
2024-12-10 13:38:36 +0800

    feat: update #base64: optimize output format

--- module/base64/encode.php
+++ module/base64/encode.php
@@ -1,9 +1,11 @@
-l<?php
+<?php
 
-	global $Queue, $Text;
-	while($nextArg = nextArg())
-		$Text = $nextArg.$Text;
-	if(strpos($Text, "[CQ:") !== false)leave("非法内容！");
-	$Queue[]= replyMessage('base64('.$Text.') = '.base64_encode($Text));
+$text = nextArg(true);
+if(!$text) {
+    replyAndLeave('不知道你想要编码什么呢…');
+}
+if(strpos($text, "[CQ:") !== false) {
+    replyAndLeave('不能包含非文本内容哦…');
+}
 
-?>
+replyAndLeave(base64_encode($text));
