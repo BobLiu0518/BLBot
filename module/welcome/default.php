490a50e3eca8b3d59e257f879582e5f7ae71352f
BobLiu <bobliu@bobliu.tech>
2024-12-10 20:35:11 +0800

    fix: update #welcome.default: fix

--- module/welcome/default.php
+++ module/welcome/default.php
@@ -1,15 +1,13 @@
 <?php
 
 global $CQ, $Event, $Text;
-if(!fromGroup()){
+if(!fromGroup()) {
         replyAndLeave("群聊中才能设置入群欢迎哦？");
 }
 
-if($CQ->getGroupMemberInfo($Event['group_id'], $Event['user_id'])->role == 'member'){
+if($CQ->getGroupMemberInfo($Event['group_id'], $Event['user_id'])->role == 'member') {
         replyAndLeave("只有管理员才能设置本群入群欢迎哦…");
 }
 
-delData('addGroupMsg/'.$Event['group_id'], ' '.$Text);
+delData('addGroupMsg/'.$Event['group_id']);
 replyAndLeave('恢复默认入群欢迎成功～');
-
-?>
