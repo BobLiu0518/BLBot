02005c065cbb33a80da6bc0f65a17bda0735410d
BobLiu <bobliu@bobliu.tech>
2024-12-10 13:30:33 +0800

    feat: update #pinyin: add no result prompt

--- module/pinyin/main.php
+++ module/pinyin/main.php
@@ -5,10 +5,8 @@ requireLvl(1);
 use Overtrue\Pinyin\Pinyin;
 
 $text = preg_replace('/\[CQ:.+?\]/', '', implode(' ', array_slice($Command, 1)).$Text);
-if(mb_strlen($text, 'UTF-8') == 1){
-	replyAndLeave(implode(' / ', Pinyin::polyphones($text)[$text]));
-}else{
-	replyAndLeave(Pinyin::fullSentence($text)->join(' '));
+if(mb_strlen($text, 'UTF-8') == 1) {
+    replyAndLeave(implode(' / ', Pinyin::polyphones($text)[$text] ?? ['未查询到结果…']));
+} else {
+    replyAndLeave(Pinyin::fullSentence($text)->join(' '));
 }
-
-?>
