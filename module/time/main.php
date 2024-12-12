fe001e74becd318d1ee89c36da65e53785ed69a2
BobLiu <bobliu@bobliu.tech>
2024-12-12 11:27:49 +0800

    feat: update #time: now convert between timestamp and time string

--- module/time/main.php
+++ module/time/main.php
@@ -1,3 +1,9 @@
 <?php
 
-replyAndLeave(date('Y/m/d H:i:s', nextArg()));
+$arg = nextArg(true);
+
+if(is_numeric($arg)) {
+    replyAndLeave(date('Y/m/d H:i:s', $arg));
+} else {
+    replyAndLeave(strtotime($arg ?: 'now'));
+}
