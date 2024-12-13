19b19e59c830da7277dff22ef80ef4e531aca76f
BobLiu <bobliu@bobliu.tech>
2024-12-13 22:22:59 +0800

    feat: reset group nickname logic

--- module/nickname/tools.php
+++ module/nickname/tools.php
@@ -4,12 +4,7 @@ function getNickname($user_id, $group_id = null, $fallback = true) {
     global $CQ, $Event;
     $nickname = getData('nickname/'.$user_id);
     if(!$nickname && $fallback) {
-        if(fromGroup() && $group_id) {
-            $user = $CQ->getGroupMemberInfo($group_id ?? $Event['group_id'], $user_id, true);
-            $nickname = $user->card ?? $user->nickname;
-        } else {
-            $nickname = $CQ->getStrangerInfo($user_id, true)->nickname;
-        }
+        $nickname = '群友';
     }
     return $nickname;
 }
