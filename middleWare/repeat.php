1a7813c121ec9f3d5c8371006ba5823367709b90
BobLiu <bobliu@bobliu.tech>
2025-01-31 20:05:11 +0800

    fix: fix repeat: reduce repeating files and other unsupported items

--- middleWare/repeat.php
+++ middleWare/repeat.php
@@ -2,30 +2,30 @@
 
 global $Event, $Message, $Queue;
 
-if(!preg_match('/^\[.+\]$/', preg_replace('/\[CQ:.+?\]/', '', $Message)) && !preg_match('/^(\[.+\])?\//', $Message)){
-    function parsePicId($str){
+if(!preg_match('/^\[.+\]$/', preg_replace('/\[CQ:(emoji|face|emoji|at).+?\]/', '', $Message)) && !preg_match('/^(\[.+\])?\//', $Message)) {
+    function parsePicId($str) {
         return preg_replace('/\[CQ:image,file=.+?fileid=(.+?)_.+?\]/', '$1', $str);
     }
 
     $recent = [];
     $current = -1;
-    foreach(range(1, 4) as $i){
+    foreach(range(1, 4) as $i) {
         $msg = getData('repeat/'.$Event['group_id'].'-'.$i);
-        if(!$msg && $current == -1 && $i != 4){
+        if(!$msg && $current == -1 && $i != 4) {
             $current = $i;
-        }else{
+        } else {
             $recent[] = parsePicId($msg);
         }
     }
-    if($current != -1){
+    if($current != -1) {
         setData('repeat/'.$Event['group_id'].'-'.$current, $Message, true);
         setData('repeat/'.$Event['group_id'].'-'.($current % 3 + 1), '');
         setData('repeat/'.$Event['group_id'].'-4', '');
 
-        if(parsePicId($Message) == $recent[0] && parsePicId($Message) == $recent[1] && parsePicId($Message) != $recent[2]){
-            if(coolDown('repeat/'.$Event['group_id']) > 0){
+        if(parsePicId($Message) == $recent[0] && parsePicId($Message) == $recent[1] && parsePicId($Message) != $recent[2]) {
+            if(coolDown('repeat/'.$Event['group_id']) > 0) {
                 coolDown('repeat/'.$Event['group_id'], 60);
-                $Queue[]= sendBack($Message);
+                $Queue[] = sendBack($Message);
             }
             setData('repeat/'.$Event['group_id'].'-4', $Message, true);
             leave();
@@ -33,4 +33,3 @@ if(!preg_match('/^\[.+\]$/', preg_replace('/\[CQ:.+?\]/', '', $Message)) && !pre
     }
 }
 
-?>
