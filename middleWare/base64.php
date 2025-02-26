f02a3fbaa6931927cb318e58ab106c06317640ca
BobLiu <bobliu@bobliu.tech>
2025-02-26 13:21:08 +0800

    feat: new feature: auto parse base64

--- middleWare/base64.php
+++ middleWare/base64.php
@@ -2,6 +2,6 @@
 
 global $Message;
 
-if(preg_match('/^[A-Za-z0-9\+\/]+={0,2}$/', $Message)) {
-    replyAndLeave(base64_decode($Message));
+if(strlen($Message) >= 18 && preg_match('/^[A-Za-z0-9\+\/]+={0,2}$/', $Message) && $result = base64_decode($Message)) {
+    replyAndLeave("Base64 解析结果：\n$result");
 }
