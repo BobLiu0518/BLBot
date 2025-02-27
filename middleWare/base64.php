0be6ab1882bef60651d412f1e62964264d874171
BobLiu <bobliu@bobliu.tech>
2025-02-27 18:16:30 +0800

    fix: avoid base64 message inject

--- middleWare/base64.php
+++ middleWare/base64.php
@@ -3,5 +3,5 @@
 global $Message;
 
 if(strlen($Message) >= 18 && preg_match('/^[A-Za-z0-9\+\/]+={0,2}$/', $Message) && $result = base64_decode($Message)) {
-    replyAndLeave("Base64 解析结果：\n$result");
+    replyAndLeave("Base64 解析结果：\n".preg_replace('/\[CQ:.+?\]/', '', $result));
 }
