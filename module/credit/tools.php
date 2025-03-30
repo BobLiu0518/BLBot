be8f53a9919eb150dc450754019ff900b685eebf
BobLiu <bobliu@bobliu.tech>
2025-03-30 23:58:00 +0800

    style: remove useless comments & format code

--- module/credit/tools.php
+++ module/credit/tools.php
@@ -1,8 +1,6 @@
 <?php
 
-//leave('服务器迁移中，金币功能关闭，想加速迁移服务器可以看我空间给我打钱');
-
-function getCredit($QQ){
+function getCredit($QQ) {
     return (int)getData("credit/{$QQ}");
 }
 
@@ -11,25 +9,23 @@ function setCredit($QQ, $credit, $set = false){
     return setData("credit/{$QQ}", (int)$credit);
 }
 
-function addCredit($QQ, $income){
+function addCredit($QQ, $income) {
     setData('credit.history', "+ {$QQ} {$income}\n", true);
     return setCredit($QQ, getCredit($QQ)+(int)$income, true);
 }
 
-function decCredit($QQ, $pay, $force = false){
+function decCredit($QQ, $pay, $force = false) {
     $balance = getCredit($QQ);
-    if($balance >= $pay || $force){
+    if($balance >= $pay || $force) {
         setData('credit.history', "- {$QQ} {$pay}\n");
-        return setCredit($QQ, (int)($balance-$pay), true);
-    }else{
-        if($balance == 0)replyAndLeave('余额不足，签到即可获取金币哦！');
-        replyAndLeave('余额不足，还需要 '.($pay-$balance).' 个金币哦，多多签到获取金币吧！');
+        return setCredit($QQ, (int)($balance - $pay), true);
+    } else {
+        if($balance == 0) replyAndLeave('余额不足，签到即可获取金币哦！');
+        replyAndLeave('余额不足，还需要 '.($pay - $balance).' 个金币哦，多多签到获取金币吧！');
     }
 }
 
-function transferCredit($from, $to, $transfer, $fee = 1.01){
+function transferCredit($from, $to, $transfer, $fee = 1.01) {
     decCredit($from, ceil($transfer * $fee));
     addCredit($to, $transfer);
 }
-
-?>
