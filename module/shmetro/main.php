f3fa56d554a105c69e053f9c17a903b3ff45c202
BobLiu <bobliu@bobliu.tech>
2025-05-23 13:48:20 +0800

    feat: new feature: #shmetro

---
+++ module/shmetro/main.php
@@ -0,0 +1,29 @@
+<?php
+
+requireLvl(2);
+
+$stations = json_decode(file_get_contents('https://m.shmetro.com/core/shmetro/mdstationinfoback_new.ashx?act=getAllStations'), true);
+$stationName = nextArg();
+if (!$stationName) replyAndLeave('不知道你要查询什么车站呢…');
+
+$pics = [];
+$result = '';
+foreach ($stations as $station) {
+  if (trim($station['value']) == $stationName) {
+    $code = $station['key'];
+    $pic = getCache("shmetro/{$code}.png");
+    if (!$pic) {
+      $pic = file_get_contents("https://service.shmetro.com/skin/zct/{$code}.jpg");
+      setCache("shmetro/{$code}.png", $pic);
+    }
+    if (!in_array($pic, $pics)) {
+      $pics[] = $pic;
+      $result .= sendImg($pic);
+    }
+  }
+}
+
+if (!$result) {
+  replyAndLeave('未找到结果…');
+}
+replyAndLeave("{$stationName} 站层图：\n{$result}");
