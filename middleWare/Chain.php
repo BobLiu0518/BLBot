6b0d4d65b78479f54de7b74cf0c675d35b913f01
BobLiu <bobliu@bobliu.tech>
2025-09-08 12:21:58 +0800

    fix: fix removed middleWare not deleted from code

--- middleWare/Chain.php
+++ middleWare/Chain.php
@@ -15,7 +15,6 @@ require('base64.php');
 require('calc.php');
 require('choose.php');
 require('v50.php');
-require('wakeup.php');
 // require('song.php');
 require('repeat.php');
 require('help.php');
