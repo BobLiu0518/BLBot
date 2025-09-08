1d84bca41ac9396db9f7ef6e1e6ff86ceea17b10
BobLiu <bobliu@bobliu.tech>
2025-09-08 16:34:21 +0800

    feat: update #schedule: optimize xiaoai import

--- module/schedule/xiaoai.php
+++ module/schedule/xiaoai.php
@@ -2,6 +2,7 @@
 
 global $Event, $Command, $Text;
 loadModule('schedule.tools');
+loadModule('schedule.xiaoai.tools');
 
 $name = null;
 $semesterStart = null;
@@ -11,8 +12,8 @@ $link = trim(implode(' ', array_splice($Command, 1)).$Text);
 if(!$link) {
     replyAndLeave(<<<EOT
 设置课程表方法：
-1. 打开小爱课程表，导入或手动录入自己的课表信息，非小米设备可下载小爱同学或小爱课程表 app：
-https://zhengy7.lanzoue.com/i9wkK0mx1fhe
+1. 打开小爱课程表，导入或手动录入自己的课表信息，非小米设备可下载小爱同学或小爱课程表 app；
+注：关于如何绕过下线提示、强制从教务导入课表的方法，请自行搜寻；
 2. 在课程表设置中，选择分享课表，复制分享链接；
 3. 发送指令 /schedule.xiaoai <分享链接>，注意指令中不包含括号。
 EOT);
@@ -20,8 +21,7 @@ EOT);
     replyAndLeave('这好像不是小爱课表的链接哦…');
 }
 $params = explode('%26', base64_decode($matches[1]));
-$api = "https://i.ai.mi.com/course-multi/table?ctId={$params[4]}&userId={$params[0]}&deviceId={$params[1]}";
-$data = json_decode(file_get_contents($api), true)['data'];
+$data = (new AiSchedule())->getCourseTable($params[2]);
 if(!$data) replyAndLeave('读取失败，可能是链接无效…');
 
 $name = $data['name'];
