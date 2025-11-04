258731a57841b76df39958da563042413fef2c7e
BobLiu <bobliu@bobliu.tech>
2025-11-04 15:25:09 +0800

    feat: update #nickname.tools: adapt napcat

--- module/nickname/tools.php
+++ module/nickname/tools.php
@@ -6,7 +6,7 @@ function getNickname($user_id, $group_id = null, $fallback = true) {
     if(!$nickname && $fallback) {
         if(fromGroup() && $group_id) {
             $user = $CQ->getGroupMemberInfo($group_id ?? $Event['group_id'], $user_id, true);
-            $nickname = $user->card ?? $user->nickname;
+            $nickname = $user->card ? $user->card : $user->nickname;
         } else {
             $nickname = $CQ->getStrangerInfo($user_id, true)->nickname;
         }
